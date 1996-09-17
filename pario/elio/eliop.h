dbd6d06ceb36bcd042d585ed8da04ed8c7a1ae24
GA Developer <hpctools@googlegroups.com>
1996-09-17 22:27:30 +0000

    JN:  private header file shared among elio files

---
+++ pario/elio/eliop.h
@@ -0,0 +1,40 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#if (defined(SP) || defined(SP1)) && !defined(NOPIOFS)
+#define PIOFS 1
+#endif
+#if !defined(SGI) && (defined(SGITFP) || defined(SGI64))
+#define SGI 1
+#endif
+
+
+#if (defined(SUN) && !defined(SOLARIS)) || defined(LINUX)
+#        include <sys/vfs.h>
+#        define  STATVFS statfs
+#elif defined(CRAY) || defined(AIX)
+#        include <sys/statfs.h>
+#        define  STATVFS statfs
+#elif !defined(PARAGON)
+#        include <sys/statvfs.h>
+#        define  STATVFS statvfs
+#endif
+
+#include <unistd.h>
+#include <fcntl.h>
+#if defined(PARAGON)
+#  include <sys/mount.h>
+#  include <nx.h>
+#endif
+#if defined(SP) || defined(SP1)
+#  include <piofs/piofs_ioctl.h>
+#endif
+
+
+#include "elio.h"
+#include "pablo.h"
+
+extern int                   _elio_Errors_Fatal;
