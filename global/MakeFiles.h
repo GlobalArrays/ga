d580d2ad983d2e6cb238fbf8141c90466e823a97
GA Developer <hpctools@googlegroups.com>
1997-02-04 21:57:33 +0000

    fixes ga_lock problem in build on SOLARIS rak with JN approval

--- global/MakeFiles.h
+++ global/MakeFiles.h
@@ -44,7 +44,7 @@ else
      endif
 endif
 GA_CORE := global.core.o global.util.o global.patch.o global.msg.o \
-           global.serv.o
+           global.serv.o ga_lock.o
 
 ifdef IWAY
   GA_CORE += iway.o net.o
