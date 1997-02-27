5505d523947cd4a24f8112d439ae6031e15cbf97
GA Developer <hpctools@googlegroups.com>
1997-02-27 17:03:47 +0000

    tps: LINUX target back to using f2c rather than g77

--- global/Makefile.h
+++ global/Makefile.h
@@ -57,23 +57,11 @@ endif
 #................................ LINUX ....................................
 # IBM ThinkPad running Linux 1.2.13
 #
-ifeq ($(TARGET),LINUX_F2C)
+ifeq ($(TARGET),LINUX)
     MAKEFLAGS = -j 1
     EXPLICITF = TRUE
  GLOB_DEFINES = -DLINUX
           CPP = gcc -E -nostdinc -undef -P
-       RANLIB = ranlib
-endif
-#
-#................................ LINUX ....................................
-# Linux 2.0.28 running g77
-#
-ifeq ($(TARGET),LINUX)
-           FC = g77
-         FOPT = -O -fno-second-underscore
-    MAKEFLAGS = -j 1
- GLOB_DEFINES = -DLINUX
-          CPP = g77 -E -nostdinc -undef -P
        RANLIB = ranlib
 endif
 #
