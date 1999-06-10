b0b2d9def13e52c078703c2ceb38fd141198887f
GA Developer <hpctools@googlegroups.com>
1999-06-10 20:21:58 +0000

    JN: TARGET_CPU for SGI

--- armci/config/makefile.h
+++ armci/config/makefile.h
@@ -41,6 +41,7 @@ ifeq ($(TARGET),SOLARIS)
    EXTRA_LIBS = /usr/ucblib/libucb.a -lsocket -lrpcsvc -lnsl
 endif
 
+#----------------------------- HP/Convex ------------------------------
 ifeq ($(TARGET),HPUX)
 # HP compiler can generate bad code for shared memory data
 # use gcc if cc breaks
@@ -72,47 +73,64 @@ ifeq ($(TARGET),CONVEX-SPP)
 endif
 
 
+#----------------------------- SGI ---------------------------------
 ifeq ($(TARGET),SGI)
+    COPT_REN = -n32 -mips4
+    FOPT_REN = -n32 -mips4
     SGI = yes
 endif
 
 ifeq ($(TARGET),SGI_N32)
-     SGI = yes
-endif
-
-ifdef SGI
-    ifeq ($(FOPT),-O)
-         FOPT = -O3
-    endif
     COPT_REN = -n32 -mips4
     FOPT_REN = -n32 -mips4
-GLOB_DEFINES += -DSGI
+    SGI = yes
 endif
 
-
 ifeq ($(TARGET),SGI64)
-     ifeq ($(FOPT),-O)
-         FOPT = -O3
-     endif
-     COPT_REN = -64 -mips4
-     FOPT_REN = -align64 -64 -mips4 -OPT:IEEE_arithmetic=2:fold_arith_limit=4000
- GLOB_DEFINES +=-DSGI
+    COPT_REN = -64 -mips4
+    FOPT_REN = -align64 -64 -mips4 
+    SGI = yes
 endif
 
 ifeq ($(TARGET),SGITFP)
-     ifeq ($(FOPT),-O)
-         FOPT = -O3
-     endif
-     COPT_REN = -64 -mips4
-     FOPT_REN = -align64 -64 -mips4 -OPT:IEEE_arithmetic=2:fold_arith_limit=4000
- GLOB_DEFINES +=-DSGI
+    COPT_REN = -64 -mips4
+    FOPT_REN = -align64 -64 -mips4 
+    SGI = yes
 endif
 
+ifdef SGI
+GLOB_DEFINES += -DSGI
+# optimization flags for R10000 (IP28)
+  FOPT_R10K = -TENV:X=1 -WK,-so=1,-o=1,-r=3,-dr=AKC
+# optimization flags for R8000 (IP21)
+  FOPT_R8K = -TENV:X=3 -WK,-so=1,-o=1,-r=3,-dr=AKC
+    ifeq ($(FOPT),-O)
+         FOPT = -O3
+    endif
+
+#CPU specific compiler flags
+ifdef TARGET_CPU
+
+ifeq ($(TARGET_CPU),R10000)
+ FOPT_REN += $(FOPT_R10K)
+endif
+ifeq ($(TARGET_CPU),R8000)
+ FOPT_REN += $(FOPT_R8K)
+endif
+
+else
+    FOPT_REN += $(FOPT_R10K)
+endif
+
+endif
+
+#----------------------------- DEC/Compaq ---------------------------------
 ifeq ($(TARGET),DECOSF)
           CLD = cc
 endif
 
 
+#------------------------------- Crays ------------------------------------
 ifeq ($(TARGET),CRAY-YMP)
      ifeq ($(FOPT), -O)
          FOPT = -O1
@@ -141,19 +159,7 @@ ifeq ($(TARGET),CRAY-T3E)
  GLOB_DEFINES = -DCRAY_T3E
 endif
 
-
-ifeq ($(TARGET),PARAGON)
-     FOPT_REN = -nx
-     COPT_REN = -nx -Msafeptr
-           FC = if77
-           CC = icc
-           AR = ar860
-           AS = as860
-     FOPT_REN += -Knoieee -Mquad -Mreentrant -Mrecursive
-     COPT_REN += -Knoieee -Mquad -Mreentrant
- GLOB_DEFINES += -DNX
-endif
-
+#................................. IBM SP and workstations ...................
 
 ifeq ($(TARGET),LAPI)
          IBM  = 1
@@ -179,6 +185,19 @@ ifdef IBM
 GLOB_DEFINES  += -DAIX
 endif
 
+#...........................
+
+ifeq ($(TARGET),PARAGON)
+     FOPT_REN = -nx
+     COPT_REN = -nx -Msafeptr
+           FC = if77
+           CC = icc
+           AR = ar860
+           AS = as860
+     FOPT_REN += -Knoieee -Mquad -Mreentrant -Mrecursive
+     COPT_REN += -Knoieee -Mquad -Mreentrant
+ GLOB_DEFINES += -DNX
+endif
 
        DEFINES = $(GLOB_DEFINES) $(LIB_DEFINES)
 
