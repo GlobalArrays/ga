634f924a07265dcffb1b4923aa5ed0c1301ffda4
GA Developer <hpctools@googlegroups.com>
1999-08-16 18:58:27 +0000

    JJU: tried prefetching

--- armci/src/fcopy.f
+++ armci/src/fcopy.f
@@ -21,6 +21,7 @@
       do c = 1, cols
       r1 = iand(max0(rows,0),3)
       do r = 1, r1
+c$$$         b(r,c) = a(r,c) + b(r,c) * 0
          b(r,c) = a(r,c)
       end do
       do r = r1 + 1, rows, 4
@@ -32,6 +33,10 @@
          b(r+1,c) = d2
          b(r+2,c) = d3
          b(r+3,c) = d4
+c$$$         b(r,c) = a(r,c) + b(r,c) * 0
+c$$$         b(r+1,c) = a(r+1,c) + b(r+1,c) * 0
+c$$$         b(r+2,c) = a(r+2,c) + b(r+2,c) * 0
+c$$$         b(r+3,c) = a(r+3,c) + b(r+3,c) * 0
       enddo
       enddo
       end
