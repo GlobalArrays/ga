5177dd685a9cf07c4bde9d7512d0c774f3eace4a
GA Developer <hpctools@googlegroups.com>
1999-10-14 00:21:12 +0000

    JN: deleted unused variables + added cvs id

--- pario/sf/shared.files.c
+++ pario/sf/shared.files.c
@@ -1,3 +1,27 @@
+/* $Id: shared.files.c,v 1.6 1999-10-14 00:21:12 d3h325 Exp $ */
+/* DISCLAIMER
+ *
+ * This material was prepared as an account of work sponsored by an
+ * agency of the United States Government.  Neither the United States
+ * Government nor the United States Department of Energy, nor Battelle,
+ * nor any of their employees, MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
+ * ASSUMES ANY LEGAL LIABILITY OR RESPONSIBILITY FOR THE ACCURACY,
+ * COMPLETENESS, OR USEFULNESS OF ANY INFORMATION, APPARATUS, PRODUCT,
+ * SOFTWARE, OR PROCESS DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT
+ * INFRINGE PRIVATELY OWNED RIGHTS.
+ *
+ *
+ * ACKNOWLEDGMENT
+ *
+ * This software and its documentation were produced with United States
+ * Government support under Contract Number DE-AC06-76RLO-1830 awarded by
+ * the United States Department of Energy.  The United States Government
+ * retains a paid-up non-exclusive, irrevocable worldwide license to
+ * reproduce, prepare derivative works, perform publicly and display
+ * publicly by or for the US Government, including the right to
+ * distribute to other US Government contractors.
+ */
+
 #include "elio.h"
 #include "sf.h"
 
