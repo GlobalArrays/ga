3363c87f6e7913eea069226d0ab23eb49114af86
GA Developer <hpctools@googlegroups.com>
2001-01-18 22:24:33 +0000

    JN: test&set for alpha

---
+++ armci/src/tas-alpha.h
@@ -0,0 +1,51 @@
+/* Machine-dependent pthreads configuration and inline functions.
+   Alpha version.
+Copyright (C) 1996 Free Software Foundation, Inc.
+This file is part of the GNU C Library.
+Contributed by Richard Henderson <rth@tamu.edu>.
+
+The GNU C Library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public License as
+published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.
+
+The GNU C Library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with the GNU C Library; see the file COPYING.LIB.  If
+not, write to the Free Software Foundation, Inc., 675 Mass Ave,
+Cambridge, MA 02139, USA.  */
+
+#include <asm/pal.h>
+
+
+/* Spinlock implementation; required.  */
+static inline long testandset(int *spinlock)
+{
+  long ret, temp;
+
+  __asm__ __volatile__(
+        "/* Inline spinlock test & set */\n"
+        "1:\t"
+        "ldl_l %0,%3\n\t"
+        "bne %0,2f\n\t"
+        "or $31,1,%1\n\t"
+        "stl_c %1,%2\n\t"
+        "beq %1,1b\n"
+        "2:\tmb\n"
+        "/* End spinlock test & set */"
+        : "=&r"(ret), "=&r"(temp), "=m"(*spinlock)
+        : "m"(*spinlock)
+        : "memory");
+
+  return ret;
+}
+
+/* Spinlock release; default is just set to zero.  */
+#define RELEASE_SPINLOCK(spinlock) \
+  __asm__ __volatile__("mb" : : : "memory"); \
+  *spinlock = 0
+
