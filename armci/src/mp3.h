eef9c922f5ebe333ed73f532db345723037b0c61
GA Developer <hpctools@googlegroups.com>
2001-02-14 22:08:10 +0000

    JN:  message passing portability wrappers

---
+++ armci/src/mp3.h
@@ -0,0 +1,31 @@
+#if defined(PVM)
+#   include <pvm3.h>
+#   ifdef CRAY
+#     define MPGROUP         (char *)NULL
+#     define MP_INIT(arc,argv)
+#   else
+#     define MPGROUP           "mp_working_group"
+#     define MP_INIT(arc,argv) pvm_init(arc, argv)
+#   endif
+#   define MP_FINALIZE()     pvm_exit()
+#   define MP_BARRIER()      pvm_barrier(MPGROUP,-1)
+#   define MP_MYID(pid)      *(pid)   = pvm_getinst(MPGROUP,pvm_mytid())
+#   define MP_PROCS(pproc)   *(pproc) = (int)pvm_gsize(MPGROUP)
+    void pvm_init(int argc, char *argv[]);
+#elif defined(TCGMSG)
+#   include <sndrcv.h>
+    long tcg_tag =30000;
+#   define MP_BARRIER()      SYNCH_(&tcg_tag)
+#   define MP_INIT(arc,argv) PBEGIN_((argc),(argv))
+#   define MP_FINALIZE()     PEND_()
+#   define MP_MYID(pid)      *(pid)   = (int)NODEID_()
+#   define MP_PROCS(pproc)   *(pproc) = (int)NNODES_()
+#else
+#   include <mpi.h>
+#   define MP_BARRIER()      MPI_Barrier(MPI_COMM_WORLD)
+#   define MP_FINALIZE()     MPI_Finalize()
+#   define MP_INIT(arc,argv) MPI_Init(&(argc),&(argv))
+#   define MP_MYID(pid)      MPI_Comm_rank(MPI_COMM_WORLD, (pid))
+#   define MP_PROCS(pproc)   MPI_Comm_size(MPI_COMM_WORLD, (pproc));
+#endif
+
