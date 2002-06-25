081046d0e58538eba386de920733410e7ee28c6d
GA Developer <hpctools@googlegroups.com>
2002-06-25 21:39:05 +0000

    VT: fixed bug serverlock should be defined when server thread is defined.

--- armci/src/mutex.c
+++ armci/src/mutex.c
@@ -1,4 +1,4 @@
-/* $Id: mutex.c,v 1.16 2002-06-25 17:17:34 vinod Exp $ */
+/* $Id: mutex.c,v 1.17 2002-06-25 21:39:05 vinod Exp $ */
 #include "armcip.h"
 #include "copy.h"
 #include "request.h"
@@ -8,7 +8,7 @@
 #define MAX_LOCKS 32768
 #define SPINMAX 1000
 
-#ifdef LAPI || defined(SERVER_THREAD) 
+#if defined(LAPI) || defined(SERVER_THREAD) 
 #  define SERVER_LOCK 
 #endif
 
