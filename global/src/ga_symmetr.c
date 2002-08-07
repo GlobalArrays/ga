2dc1005ed95f9637a12a9df7b4e44ec1be72dbd5
GA Developer <hpctools@googlegroups.com>
2002-08-07 18:28:52 +0000

    VT: modified brocken logic

--- global/src/ga_symmetr.c
+++ global/src/ga_symmetr.c
@@ -9,40 +9,31 @@
 #include "globalp.h"
 #include "macdecls.h"
 
-#  define COPYINDEX_F2C(farr, carr, n){\
-   int i; for(i=0; i< (n); i++)(carr)[n-i-1]=(int)(farr)[i] -1;}
-
 void FATR 
 gai_add(Integer *lo, Integer *hi, Void *a, Void *b, DoublePrecision alpha,
 	Integer type, Integer nelem, Integer ndim) {
 
-  Integer alo[GA_MAX_DIM], ahi[GA_MAX_DIM];
-  Integer f, i, j=0, k=0, m=0;
-  Integer offset = 1; /* 2-d offset */
-  Integer nrow, ncol, indexA=0;
+  Integer i, j, m=0;
+  Integer nrow, ncol, indexA=0, indexB=0;
   DoublePrecision *A = (DoublePrecision *)a, *B = (DoublePrecision*)b;
+  Integer offset1=1, offset2=1;
+
+  nrow = hi[ndim-2] - lo[ndim-2] + 1;
+  ncol = hi[ndim-1] - lo[ndim-1] + 1;
   
-  COPYINDEX_F2C(lo, alo, ndim);
-  COPYINDEX_F2C(hi, ahi, ndim); 
- 
-  nrow = ahi[ndim-2]-alo[ndim-2]+1;  
-  ncol = ahi[ndim-1]-alo[ndim-1]+1;
-  offset = nrow*ncol;
-
-  for(f=0; f<nelem; f+=offset) {
-    j=0; k=0; m=0;
-    for(i=0; i<nrow*ncol; i++) {
-	indexA=k+j*nrow;
-	if(indexA>=nrow*ncol) {
-	  j=0; k++; 
-	  indexA=k+j*ncol;
-	}
-	A[f+indexA] = alpha *(A[f+indexA] + B[f+i]);
-	j++;
+  for(i=0; i<ndim-2; i++) {
+    offset1 *= hi[i] - lo[i] + 1;
+    offset2 *= hi[i] - lo[i] + 1;
+  }
+  offset1 *= nrow;
+  
+  for(j=0; j<offset2; ++j,indexA=j,indexB=j,m=0) {
+    for(i=0; i<nrow*ncol; i++, indexA += offset1, indexB += offset2) {
+      if(indexA >= nelem) indexA = j + ++m*offset2;
+      A[indexA] = alpha *(A[indexA] + B[indexB]);
     }
-  }  
+  }
 }
-
 
 void FATR 
 ga_symmetrize_(Integer *g_a) {
@@ -56,9 +47,14 @@ ga_symmetrize_(Integer *g_a) {
   Integer g_b; /* temporary global array (b = A') */
   Void *a_ptr, *b_ptr;
   Integer bindex;
-  
+
   ga_sync_();
   GA_PUSH_NAME("nga_copy_patch");
+
+#ifdef T_CODE
+  ga_symmetrizee_(g_a);
+#else  
+
   
   nga_inquire_internal_(g_a, &type, &ndim, dims);
   
@@ -72,38 +68,35 @@ ga_symmetrize_(Integer *g_a) {
   have_data = ahi[0]>0;
   for(i=1; i<ndim; i++) have_data = have_data && ahi[i]>0;
   
-  /*ga_print_(g_a);*/
-  
   if(have_data) {
     nga_access_ptr(g_a, alo, ahi, &a_ptr, lda); 
     
     for(i=0; i<ndim; i++) nelem *= ahi[i]-alo[i] +1;
     if(!MA_push_get(MT_F_DBL, nelem, "v", &g_b, &bindex) ||
-       !MA_get_pointer(g_b, &b_ptr)) 
+       !MA_get_pointer(g_b, &b_ptr)) /* @ check MT_F_DBL @ */
       ga_error(" MA Failed: insufficient memory ", nelem);
     
     
-    for(i=2; i<ndim; i++) {bhi[i]=ahi[i]; blo[i]=alo[i]; ldb[i]=lda[i]; }
+    for(i=0; i<ndim-2; i++) {bhi[i]=ahi[i]; blo[i]=alo[i]; }
     
     /* switch rows and cols */
-    blo[0]=alo[1];
-    bhi[0]=ahi[1];
-    blo[1]=alo[0];
-    bhi[1]=ahi[0];
+    blo[ndim-1]=alo[ndim-2];
+    bhi[ndim-1]=ahi[ndim-2];
+    blo[ndim-2]=alo[ndim-1];
+    bhi[ndim-2]=ahi[ndim-1];
 
     for (i=0; i < ndim-1; i++) 
-      ldb[i] = bhi[i] - blo[i] + 1;/* as I switched rows and cols */
+      ldb[i] = bhi[i] - blo[i] + 1; 
     nga_get_(g_a, blo, bhi, b_ptr, ldb);
   }
-  ga_sync_(); /* why ? check */
+  ga_sync_(); 
 
   if(have_data) {
     gai_add(alo, ahi, a_ptr, b_ptr, alpha, type, nelem, ndim);
     nga_release_update_(g_a, alo, ahi);
     if (!MA_pop_stack(g_b)) ga_error("MA_pop_stack failed",0);
   }
-
+#endif
   GA_POP_NAME;
   ga_sync_();
 }
-
