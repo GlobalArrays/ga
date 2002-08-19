5935d6fdc93a65c5077f6e9d1d2315482da02ab7
GA Developer <hpctools@googlegroups.com>
2002-08-19 17:13:33 +0000

    VT: tcgmsg uses armci_barrier on lapi

--- tcgmsg/ipcv5.0/synch.c
+++ tcgmsg/ipcv5.0/synch.c
@@ -1,10 +1,14 @@
 #include "tcgmsgP.h"
 
-
 void SYNCH_(Integer *ptype)
 {
   Integer junk = 0, n = 1;
-
+#ifdef LAPI
+  extern int _armci_called_from_barrier;
+  if(!_armci_called_from_barrier)
+    armci_msg_barrier();
+  else
+#endif
   IGOP_(ptype, &junk, &n, "+");
 
 }
