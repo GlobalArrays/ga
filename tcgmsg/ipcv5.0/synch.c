c4b69914cb5858c3c71e6d3cdb792cdc7127e8a2
GA Developer <hpctools@googlegroups.com>
2002-08-19 21:41:29 +0000

    VT: shouldnt touch tcgmsg. Change was to be made in GA

--- tcgmsg/ipcv5.0/synch.c
+++ tcgmsg/ipcv5.0/synch.c
@@ -3,12 +3,7 @@
 void SYNCH_(Integer *ptype)
 {
   Integer junk = 0, n = 1;
-#ifdef LAPI
-  extern int _armci_called_from_barrier;
-  if(!_armci_called_from_barrier)
-    armci_msg_barrier();
-  else
-#endif
+
   IGOP_(ptype, &junk, &n, "+");
 
 }
