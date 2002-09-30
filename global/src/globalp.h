41d95f14dc1ad7b6cb5f7ea781541676807d3cc4
GA Developer <hpctools@googlegroups.com>
2002-09-30 17:26:01 +0000

    added xgemm function prototypes.

--- global/src/globalp.h
+++ global/src/globalp.h
@@ -167,4 +167,16 @@ extern logical ngai_comp_patch(Integer andim, Integer *alo, Integer *ahi,
 extern logical ngai_test_shape(Integer *alo, Integer *ahi, Integer *blo,
                           Integer *bhi, Integer andim, Integer bndim);
 
+extern void xb_sgemm (char *transa, char *transb, int *M, int *N, int *K,
+		      float *alpha, const float *a, int *p_lda,const float *b,
+		      int *p_ldb, float *beta, float *c, int *p_ldc);
+
+extern void xb_dgemm (char *transa, char *transb, int *M, int *N, int *K,
+		      double *alpha,const double *a,int *p_lda,const double *b,
+		      int *p_ldb, double *beta, double *c, int *p_ldc);
+
+extern void xb_zgemm (char * transa, char *transb, int *M, int *N, int *K,
+		      const void *alpha,const void *a,int *p_lda,const void *b,
+		      int *p_ldb, const void *beta, void *c, int *p_ldc);
+
 #endif
