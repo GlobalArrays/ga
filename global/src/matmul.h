46adc7c65fca214812575d37e9f0cb8466d63c2c
GA Developer <hpctools@googlegroups.com>
2003-07-31 23:54:47 +0000

    MKK: New matmul implementation that incorporates data locality, dynamic load balancing and non-blocking operations

---
+++ global/src/matmul.h
@@ -0,0 +1,94 @@
+#ifndef _MATMUL_H_
+#define _MATMUL_H_
+
+#include "ga.h"
+#include "global.h"
+#include "globalp.h"
+#include "base.h"
+
+#include <math.h>
+#ifdef WIN32
+#include <armci.h>
+#else
+#include <../../armci/src/armci.h>
+#endif
+
+#ifdef KSR
+#  define dgemm_ sgemm_
+#  define zgemm_ cgemm_
+#endif
+
+#ifdef CRAY
+#      include <fortran.h>
+#      define  DGEMM SGEMM
+#      define  ZGEMM CGEMM
+#elif defined(WIN32)
+extern void FATR DGEMM(char*,int, char*,int, Integer*, Integer*, Integer*,
+		       void*, void*, Integer*, void*, Integer*, void*,
+		       void*, Integer*);
+extern void FATR ZGEMM(char*,int, char*,int, Integer*, Integer*, Integer*,
+		       DoubleComplex*, DoubleComplex*, Integer*, DoubleComplex*,
+		       Integer*, DoubleComplex*, DoubleComplex*, Integer*);
+#elif defined(F2C2__)
+#      define DGEMM dgemm__
+#      define ZGEMM zgemm__
+#elif defined(HITACHI)
+#      define dgemm_ DGEMM
+#      define zgemm_ ZGEMM
+#endif
+
+#if defined(CRAY) || defined(WIN32)
+#   define cptofcd(fcd)  _cptofcd((fcd),1)
+#else
+#      define cptofcd(fcd) (fcd)
+#endif
+#ifdef STATBUF
+#  define C_CHUNK  92 
+#  define D_CHUNK  64
+#  define ICHUNK C_CHUNK
+#  define JCHUNK C_CHUNK
+#  define KCHUNK C_CHUNK
+#else
+/* min acceptable amount of memory (in elements) and default chunk size */
+#  define MINMEM 64
+#  define CHUNK_SIZE 128
+#  define MAX_CHUNKS 1024
+#  define NUM_MATS 3 
+#  define MINTASKS 6 /* increase this if there is high load imbalance */
+#  define EXTRA 4
+#endif
+
+#ifdef MPI
+#  include "mpi.h"
+#  define CLOCK_ MPI_Wtime
+#else
+#include "sndrcv.h"
+#  define CLOCK_ TCGTIME_
+#endif
+
+#define SET   1
+#define UNSET 0
+
+static int gTaskId=0, _gai_matmul_patch_flag = 0; 
+Integer gNbhdlA[2], gNbhdlB[2], gNbhdlC[2];/* for A and B matrix */
+typedef struct {
+  int lo[2]; /* 2 elements: ilo and klo */
+  int hi[2];
+  int dim[2];
+  int chunkBId; 
+}task_list_t;
+
+extern void FATR  ga_nbget_(Integer *g_a, Integer *ilo, Integer *ihi, 
+			    Integer *jlo, Integer *jhi, Void *buf, 
+			    Integer *ld, Integer *nbhdl);
+
+#define VECTORCHECK(rank,dims,dim1,dim2, ilo, ihi, jlo, jhi) \
+  if(rank>2)  ga_error("rank is greater than 2",rank); \
+  else if(rank==2) {dim1=dims[0]; dim2=dims[1];} \
+  else if(rank==1) {if((ihi-ilo)>0) { dim1=dims[0]; dim2=1;} \
+                    else { dim1=1; dim2=dims[0];}} \
+  else ga_error("rank must be atleast 1",rank);
+
+#define WAIT_GET_BLOCK(nbhdl) ga_nbwait_(nbhdl)
+
+#endif /* _MATMUL_H_ */
