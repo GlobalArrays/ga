32dc28531d2442a6768924f1dd6bcd28cbd05aab
GA Developer <hpctools@googlegroups.com>
2003-10-31 22:50:15 +0000

    MKK: Increased BLOCK_SIZE from 512 to 1024

--- global/src/matmul.h
+++ global/src/matmul.h
@@ -1,4 +1,4 @@
-/* $Id: matmul.h,v 1.9 2003-10-29 02:00:33 manoj Exp $ */
+/* $Id: matmul.h,v 1.10 2003-10-31 22:50:15 manoj Exp $ */
 #ifndef _MATMUL_H_
 #define _MATMUL_H_
 
@@ -54,7 +54,7 @@ extern void FATR ZGEMM(char*,int, char*,int, Integer*, Integer*, Integer*,
 #  define MINMEM 64
 #  define CHUNK_SIZE 256
 #  define MAX_CHUNKS 1024
-#  define BLOCK_SIZE 512 /* temp buf size for pinning */
+#  define BLOCK_SIZE 1024 /* temp buf size for pinning */
 #  define NUM_MATS 3 
 #  define MINTASKS 6 /* increase this if there is high load imbalance */
 #  define EXTRA 4
