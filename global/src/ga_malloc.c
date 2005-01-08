201f72baeffdff9b10280d515a4ebcd389098f17
GA Developer <hpctools@googlegroups.com>
2005-01-08 05:28:43 +0000

    MKK: fixed the memory alginment problem with the pointer returned by MA

--- global/src/ga_malloc.c
+++ global/src/ga_malloc.c
@@ -3,7 +3,8 @@
 
 #include "globalp.h"
 #define GA_MAXMEM_AVAIL 786432  
-#define CHECK 0
+#define CHECK           0
+#define ALIGNMENT       sizeof(DoubleComplex)
 
 static void * (*ga_ext_alloc)();
 static void (*ga_ext_free)();
@@ -19,35 +20,45 @@ void* FATR ga_malloc(Integer nelem, int type, char *name) {
     void *ptr;  
     unsigned long addr;
     Integer handle, adjust=0, bytes, item_size=GAsizeofM(ga_type_f2c(type));
+    Integer extra;
 
-    nelem += sizeof(double)/item_size+1; /* extra space for storing handle */
-    
+    /* extra space for 1.ALIGNMENT and 2.storing handle */
+    if(ALIGNMENT%item_size)
+       ga_error("ga_malloc: GA datatype cannot be aligned.Adjust ALIGNMENT",0);
+    extra = 2*ALIGNMENT/item_size;
+    nelem += extra;
+
     if(ga_usesMA) { /* Uses Memory Allocator (MA) */
-       if(MA_push_stack(type,nelem,name,&handle)) MA_get_pointer(handle,&ptr);
+       if(MA_push_stack(type,nelem,name,&handle))  MA_get_pointer(handle,&ptr);
        else ga_error("ga_malloc: MA_push_stack failed",0);
+       addr = (unsigned long)ptr;
     }
     else { /* else, using external memory allocator */
-       bytes = nelem*item_size + item_size; /* extra-memory for alignment */
+       bytes = nelem*item_size;
        addr  = (unsigned long)(*ga_ext_alloc)((size_t)bytes, item_size, name);
-       /* Address Alignment: align the memory to that boundary */
-       adjust = (Integer) (addr%item_size);
-       if(adjust != 0) { adjust=item_size-adjust; addr+=adjust; }
-       ptr = (void *)addr; handle = adjust;
     }
 
+    /* Address Alignment */
+    adjust = (Integer) (addr%ALIGNMENT);
+    if(adjust != 0) { adjust=ALIGNMENT-adjust; addr+=adjust; }
+    ptr = (void *)addr; 
+    if(!ga_usesMA) handle = adjust;
+
     if(ptr == NULL) ga_error("ga_malloc failed", 0L);
-    *((Integer*)ptr)= handle;/* store handle/adjustment-value in this buffer */
-    ptr = ((double*)ptr)+ 1;  /*needs sizeof(double)>=sizeof(Integer) */
-    
+    *((Integer*)ptr)=handle;/*store handle or adjustment-value in this buffer*/
+    ptr = ((char*)ptr) + ALIGNMENT;
+
     return ptr;
 }
 
 void FATR ga_free(void *ptr) {
-    Integer handle= *( (Integer*) (-1 + (double*)ptr)); /* retreive handle */
+    Integer handle;
+    ptr = ((char*)ptr)-ALIGNMENT;
+    handle= *((Integer*)ptr); /* retreive handle */
     if(ga_usesMA) {
       if(!MA_pop_stack(handle)) ga_error("ga_free: MA_pop_stack failed",0);}
-    else {/*make sure to free original(before address alignment) pointer*/
-      ptr = (-1 + (double*)ptr); (*ga_ext_free)((char *)ptr-handle); }
+    else /*make sure to free original(before address alignment) pointer*/
+      (*ga_ext_free)((char *)ptr - handle);
 }
 
 Integer ga_memory_avail(Integer datatype) {
