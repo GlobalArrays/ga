b964dc47dac0ee4bd843a01674f7ff72f4d11211
Jeff Daily <jeff.daily@pnnl.gov>
2010-08-11 15:43:17 +0000

    wapi.c is now automatically generated from papi.h at make time. wapigen.py can now handle multi-line function signatures.

--- build-aux/wapigen.py
+++ build-aux/wapigen.py
@@ -37,29 +37,45 @@ class Param:
     def __repr__(self):
         return self.sig()
 
+accumulating = False
+acc = "" # accumulate signature while printing each line as-is
+skip = True # skip lines until the first "pnga" is found
 for line in open(sys.argv[1]): # papi.h
-    line = line.strip()
-    if line.startswith("extern"):
-        extern,type,parts = line.split(None,2)
-        extern = extern.strip()
-        type = type.strip()
+    line = line.rstrip() # remove newline and any extra trailing whitespace
+    if "#endif" in line: # avoid preprocessor guard
+        skip = True
+    if "pnga" in line:
+        skip = False
+        accumulating = True
+    if skip:
+        continue
+    if accumulating:
+        acc += line
+    if "pnga" in line:
+        line = line.replace("pnga","wnga")
+    if "extern" in line:
+        line = line.replace("extern","").lstrip()
+    if ";" in line:
+        # hit the end of the function signature, stop accumulating
+        accumulating = False
+        print line.replace(";","")
+        line = line.replace(";","")
+        extern,type,parts = acc.split(None,2)
+        acc = "" # reset accumulation buffer
         parts = parts.strip()
         pfunc,parts = parts.split("(",1)
         pfunc = pfunc.strip()
-        wfunc = "w%s" % pfunc[1:]
         parts = parts.strip().replace(";","").replace(")","").split(",")
-        sig = ""
         arg = ""
         if parts:
             param = Param(parts[0])
-            sig += param.sig()
             arg += param.arg()
         for part in parts[1:]:
             param = Param(part)
-            sig += ", %s" % param.sig()
             arg += ", %s" % param.arg()
-        print """%s %s(%s)
-{
+        print """{
     %s(%s);
 }
-""" % (type,wfunc,sig,pfunc,arg)
+""" % (pfunc,arg)
+    else:
+        print line
