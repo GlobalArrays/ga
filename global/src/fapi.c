969b1f4f40f988d27c867a9a9269c618e9db3d08
Jeff Daily <jeff.daily@pnnl.gov>
2010-12-21 19:35:14 +0000

    Fix bug in fapi scale patch.

--- global/src/fapi.c
+++ global/src/fapi.c
@@ -2642,7 +2642,7 @@ static void sga_fill_patch(Integer *g_a, Integer *ilo, Integer *ihi, Integer *jl
     Integer lo[2], hi[2];
 
     lo[0] = *ilo; lo[1] = *jlo;
-    hi[0] = *ihi; lo[1] = *jhi;
+    hi[0] = *ihi; hi[1] = *jhi;
     wnga_fill_patch(g_a, lo, hi, val);
 }
 
