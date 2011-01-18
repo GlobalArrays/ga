4aacbca111d077a6b33e17a2ea31c545f6dcd92c
Bruce Palmer <Bruce.Palmer@pnnl.gov>
2011-01-18 01:01:04 +0000

    Completed initial draft of test routine.

--- gparrays/testing/testc.c
+++ gparrays/testing/testc.c
@@ -38,15 +38,21 @@ void do_work()
 {
   int g_p, me, i, ii, j, jj, l, k;
   int m_k_ij, m_l_ij, idx;
-  int dims[2],lo[2],hi[2],ndim;
+  int dims[2], lo[2], hi[2], ndim;
+  int nelems, nsize;
   int idim, jdim, subscript[2], size;
+  int ld[2], ld_sz[2];
   int *ptr;
+  void **buf_ptr;
+  void *buf;
+  int *buf_size;
+
 
   /* Create Global Pointer array */
   dims[0] = N_I;
   dims[1] = N_J;
   ndim = 2;
-  me = GA_Nodeid();
+  me = NGA_Nodeid();
 
   g_p = GP_Create_handle();
   GP_Set_dimensions(g_p, ndim, dims);
@@ -62,7 +68,7 @@ void do_work()
     ii = i + lo[0];
     for (j=0; j<jdim; j++) {
       jj = j + lo[1];
-      idx = j*N_I + i;
+      idx = jj*N_I + ii;
       m_k_ij = ii%Q_I + 1;
       m_l_ij = jj%Q_J + 1;
       /* Allocate local memory for object and assign it values */
@@ -82,12 +88,64 @@ void do_work()
   }
   
   /* Guarantee data consistency */
-  GA_Sync();
+  NGA_Sync();
 
   /* Generate bounding coordinates to an arbitrary patch in GP array */
   get_range(ndim, dims, lo, hi);
 
-  /* Clean up Global Pointer array */
+  /* Find the total amount of data contained in the patch */
+  nsize = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
+  GP_Get_size(g_p, lo, hi, &size);
+
+  /* Allocate buffers and retrieve data */
+  buf = (void*)malloc(size);
+  buf_ptr = (void**)malloc(nsize*sizeof(void*));
+  buf_size = (int*) malloc(nsize*sizeof(int*));
+  ld[0] = hi[0]-lo[0]+1;
+  ld[1] = hi[1]-lo[1]+1;
+  ld_sz[0] = hi[0]-lo[0]+1;
+  ld_sz[1] = hi[1]-lo[1]+1;
+  GP_Get(g_p, lo, hi, buf, buf_ptr, ld, buf_size, ld_sz, &size);
+  
+  /* Check contents of buffers to see if data is as expected */
+  for (i=lo[0]; i<=hi[0]; i++) {
+    ii = i - lo[0];
+    for (j=lo[1]; j<=hi[1]; j++) {
+      jj = j - lo[1];
+      idx = j*N_I + i;
+      ptr = (int*)buf_ptr[jj*ld[0]+ii];
+      if (ptr[0] != i%Q_I + 1) {
+        NGA_Error("Element dimension i does not match",ptr[0]);
+      }
+      if (ptr[1] != j%Q_J + 1) {
+        NGA_Error("Element dimension j does not match",ptr[1]);
+      }
+      m_k_ij = i%Q_I + 1;
+      m_l_ij = j%Q_J + 1;
+      for (k=0; k<ptr[0]; k++) {
+        for (l=0; l<ptr[1]; l++) {
+          if (ptr[l*ptr[0]+k+2] != l*m_k_ij+l+idx) {
+            NGA_Error("Element ij does not match",ptr[l*ptr[0]+k+2]);
+          }
+        }
+      }
+    }
+  }
+
+  /* Clean up buffers and Global Pointer array */
+  free(buf);
+  free(buf_ptr);
+  free(buf_size);
+  GP_Distribution(g_p, me, lo, hi);
+  for (i=lo[0]; i<hi[0]; i++) {
+    subscript[0] = i;
+    for (j=lo[1]; j<hi[1]; j++) {
+      subscript[1] = j;
+      GP_Free_local_element(g_p, subscript);
+    }
+  }
+
+  /* destroy Global Pointer array */
   GP_Destroy(g_p);
 }
 
