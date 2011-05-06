5861607d1dbc0e6e738435d323e9f7b30c6dbd07
Sriram Krishnamoorthy <sriram@pnnl.gov>
2011-05-06 19:28:57 +0000

    SK: data version of tascel scf example

--- tascel/examples/scf/twoelcpp.cc
+++ tascel/examples/scf/twoelcpp.cc
@@ -161,7 +161,7 @@ static void clean_chunk(double *chunk) {
 }
 
 typedef struct {
-  long int taskid; //Integer lo[4], hi[4], it, jt, kt, lt;
+  int id;
 } task_dscr_t;
 
 typedef struct {
@@ -175,7 +175,7 @@ typedef struct {
 static void compute_lo_hi(long itask, long nbfn, long lo[4], long hi[4], 
 			 long *ilo, long *jlo, long *klo, long *llo) {
   long imax;
-  long itmp;
+  Integer itmp;
   long long total_ntsks;
   imax = nbfn/ichunk;
   if (nbfn - ichunk*imax > 0) {
@@ -259,7 +259,7 @@ static void compute_index(void *dscr, int dscr_len,
   assert(dscr_len == sizeof(task_dscr_t));
   assert(arrid >=0 && arrid<6);
   assert(idxlen ==  2*sizeof(int));
-  const long itask = ((task_dscr_t*)dscr)->taskid;
+  const long itask = ((task_dscr_t*)dscr)->id;
   int *iidx = (int *)idx;
   task_plo_t *ptplo = (task_plo_t*)pldata;
   assert(pldata_len == sizeof(task_plo_t));
@@ -303,62 +303,84 @@ static void twoel_task(UniformTaskCollection *utc, void *bigd, int bigd_len,
 		       void *pldata, int pldata_len, vector<void *> data_bufs);
 
 static void cpptwoel(Integer g_schwarz, Integer g_dens, Integer g_fock,
-		     double _schwmax, Integer nbfn,
-		     long long *_icut1, long long *_icut2,
-		     long long *_icut3, long long *_icut4) {
+		     double schwmax, Integer nbfn,
+		     long long *icut1, long long *icut2,
+		     long long *icut3, long long *icut4) {
   int dotask;
   int g_counter, ione = 1;
+  task_dscr_t tdscr;
   task_plo_t tplo;
-  tplo.nbfn = nbfn;
-  tplo.schwmax = _schwmax;
-  tplo.icut1 = _icut1;
-  tplo.icut2 = _icut2;
-  tplo.icut3 = _icut3;
-  tplo.icut4 = _icut4;
-  tplo.g_schwarz = g_schwarz;
-  tplo.g_dens = g_dens;
-  tplo.g_fock = g_fock;
 
-  const long me = GA_Nodeid();
-  const long nproc = GA_Nnodes();
-  const long imax = nbfn/ichunk + ((nbfn%ichunk) ? 1 : 0);
-  const long total_ntasks = imax*imax*imax*imax;
-  const long ntasks_per_proc = (long)ceil(1.0*total_ntasks/nproc);
-  const long tasklo = ntasks_per_proc * me;
-  const long taskhi = min(tasklo+ntasks_per_proc,total_ntasks)-1;
-  
   {
-    task_dscr_t tdscr;
+    tplo.nbfn = nbfn;
+    tplo.schwmax = schwmax;
+    tplo.icut1 = icut1;
+    tplo.icut2 = icut2;
+    tplo.icut3 = icut3;
+    tplo.icut4 = icut4;
+    tplo.g_schwarz = g_schwarz;
+    tplo.g_dens = g_dens;
+    tplo.g_fock = g_fock;
+
     TslFuncRegTbl frt;
     TslFunc tf = frt.add(twoel_task);
+    vector<DataColl*> colls;
+    int block[2] = {ichunk, ichunk};
+    DenseArray coll_schwarz(g_schwarz, block, 2);
+    DenseArray coll_dens(g_dens, block, 2);
+    DenseArray coll_fock(g_fock, block, 2);
+    /**FIXME: modes are associated with arrays and not specific data
+       movement operation*/
+    colls.push_back(&coll_schwarz);
+    colls.push_back(&coll_schwarz);
+    colls.push_back(&coll_dens);
+    colls.push_back(&coll_dens);
+    colls.push_back(&coll_fock);
+    colls.push_back(&coll_fock);
+
+    vector<AccessMode> modes(4, MODE_RONLY);
+    vector<int> idxlens(6, 2*sizeof(int));
+
+    modes.push_back(MODE_ACC);
+    modes.push_back(MODE_ACC);
+
+    const long me = GA_Nodeid();
+    const long nproc = GA_Nnodes();
+    const long imax = nbfn/ichunk + ((nbfn%ichunk) ? 1 : 0);
+    const long total_ntasks = imax*imax*imax*imax;
+    const long ntasks_per_proc = (long)ceil(1.0*total_ntasks/nproc);
+    const long tasklo = ntasks_per_proc * me;
+    const long taskhi = min(tasklo+ntasks_per_proc,total_ntasks)-1;
+
     TaskCollProps props;
-    props.functions(tf,frt)
-      .taskSize(sizeof(task_dscr_t))
-#define EVEN_DISTRIBUTION 1
+    props.functions(tf,frt).taskSize(sizeof(task_dscr_t))
+#define EVEN_DISTRIBUTION 0
 #if EVEN_DISTRIBUTION
         .maxTasks(ntasks_per_proc)
 #else
         .maxTasks(total_ntasks)
 #endif
         .localData(&tplo,sizeof(tplo));
-    UniformTaskCollectionSplit utc(props);
-    for(long i=tasklo; i<=taskhi; i++) {
-      tdscr.taskid = i;
-#if 1
-      utc.addTask(&tdscr, sizeof(tdscr));
+    UniformTaskCollSplitData utc(props, colls, modes, idxlens, compute_index);
+
+#if EVEN_DISTRIBUTION
+    for(long i=tasklo; i<=taskhi; i++)
 #else
-      vector<void *> bufs;
-      twoel_task(&utc, &tdscr, sizeof(tdscr), &tplo, sizeof(tplo), bufs);
+    for (long i=0; i<total_ntasks; ++i)
+      if (me == compute_owner(i, colls, 0, nbfn))
 #endif
-    }
+      {
+        tdscr.id = i;
+        utc.addTask(&tdscr, sizeof(tdscr));
+      }
     GA_Sync();
     utc.process();
   }
 }
 
-double f_ij[ichunk][ichunk],d_kl[ichunk][ichunk];
-double f_ik[ichunk][ichunk],d_jl[ichunk][ichunk];
-double s_ij[ichunk][ichunk],s_kl[ichunk][ichunk];
+typedef struct {
+  double buf[ichunk][ichunk];
+} tbuf_t;
 
 static void twoel_task(UniformTaskCollection *utc, void *_bigd, int bigd_len, 
 		       void *pldata, int pldata_len, vector<void *> data_bufs) {
@@ -366,10 +388,17 @@ static void twoel_task(UniformTaskCollection *utc, void *_bigd, int bigd_len,
   assert(bigd_len == sizeof(task_dscr_t));
   assert(pldata!=NULL);
   assert(pldata_len == sizeof(task_plo_t));
-  assert(data_bufs.size()==0);
+  assert(data_bufs.size()==6);
 
   task_dscr_t *ptdscr = (task_dscr_t*)_bigd;
   task_plo_t *ptplo = (task_plo_t*)pldata;
+
+  tbuf_t *s_ij = (tbuf_t*)data_bufs[0];
+  tbuf_t *s_kl = (tbuf_t*)data_bufs[1];
+  tbuf_t *d_kl = (tbuf_t*)data_bufs[2];
+  tbuf_t *d_jl = (tbuf_t*)data_bufs[3];
+  tbuf_t *f_ij = (tbuf_t*)data_bufs[4];
+  tbuf_t *f_ik = (tbuf_t*)data_bufs[5];
 
   int ich;
   Integer lch;
@@ -378,108 +407,55 @@ static void twoel_task(UniformTaskCollection *utc, void *_bigd, int bigd_len,
   double gg;
   Integer ione = 1;
   double one = 1.0;
-
-  long lo[4], hi[4], it, jt, kt, lt;
- 
   Integer nbfn;
-  double schwmax;
-  int cnt1, cnt2;
+  long lo[4], hi[4];
+  long it, jt, kt, lt;
   long long *icut1, *icut2, *icut3, *icut4;
   Integer g_schwarz, g_dens, g_fock;
+  double schwmax;
 
-  int done_get0=0, done_get1=0, accum;
-
+  long itask = ptdscr->id;
   nbfn = ptplo->nbfn;
   schwmax = ptplo->schwmax;
-  cnt1 = ptplo->cnt1;
-  cnt2 = ptplo->cnt2;
   icut1 = ptplo->icut1;
   icut2 = ptplo->icut2;
   icut3 = ptplo->icut3;
   icut4 = ptplo->icut4;
   g_schwarz = ptplo->g_schwarz;
   g_dens = ptplo->g_dens;
-  g_fock = ptplo->g_fock;
+  g_fock = ptplo->g_fock;  
 
   ld = maxnbfn;
   ich = lch = ichunk;
 
-  compute_lo_hi(ptdscr->taskid, nbfn, lo, hi, &it, &jt, &kt, &lt);
+  compute_lo_hi(itask, nbfn, lo, hi, &it, &jt, &kt, &lt);
 
-  lo_ik[0] = lo[0];
-  lo_ik[1] = lo[2];
-  hi_ik[0] = hi[0];
-  hi_ik[1] = hi[2];
-  lo_jl[0] = lo[1];
-  lo_jl[1] = lo[3];
-  hi_jl[0] = hi[1];
-  hi_jl[1] = hi[3];
-
-  assert(ich == hi_jl[0]-lo_jl[0]+1);
-  
-  {
-    /*lkji*/
-    int _lo[4] = {lo[3], lo[2], lo[1], lo[0]};
-    int _hi[4] = {hi[3], hi[2], hi[1], hi[0]};
-    int _lo_jl[2] = {lo[3], lo[1]};
-    int _hi_jl[2] = {hi[3], hi[1]};
-    NGA_Get(g_schwarz, &_lo[2], &_hi[2], s_ij, &ich);
-    //NGA_Get(g_schwarz, &_lo[0], &_hi[0], s_kl, &ich);
-    //NGA_Get(g_dens,&_lo[0],&_hi[0], d_kl,&ich);
-    //NGA_Get(g_dens,_lo_jl,_hi_jl, d_jl,&ich);
-  }
-    
-  clean_chunk((double*)f_ij);
-  clean_chunk((double *)f_ik);
-
+  assert(ich == hi[0]-lo[0]+1);
+  assert(ich == hi[2]-lo[2]+1);
+  clean_chunk((double*)f_ij->buf);
+  clean_chunk((double *)f_ik->buf);
   for(i = lo[0]; i<=hi[0]; i++) {
     iloc = i-lo[0];
     for(j = lo[1]; j<= hi[1]; j++) {
       jloc = j-lo[1];
-      if (s_ij[jloc][iloc]*(schwmax) < tol2e) {
+      if (s_ij->buf[jloc][iloc]*(schwmax) < tol2e) {
 	*icut1 = *icut1 + (hi[2]-lo[2]+1)*(hi[3]-lo[3]+1);
       }
       else {
-
-	if(done_get0==0) {
-	  int _lo[4] = {lo[3], lo[2], lo[1], lo[0]};
-	  int _hi[4] = {hi[3], hi[2], hi[1], hi[0]};
-	  int _lo_jl[2] = {lo[3], lo[1]};
-	  int _hi_jl[2] = {hi[3], hi[1]};
-	  //NGA_Get(g_schwarz, &_lo[2], &_hi[2], s_ij, &ich);
-	  NGA_Get(g_schwarz, &_lo[0], &_hi[0], s_kl, &ich);
-	  //NGA_Get(g_dens,&_lo[0],&_hi[0], d_kl,&ich);
-	  //NGA_Get(g_dens,_lo_jl,_hi_jl, d_jl,&ich);
-	  done_get0=1;
-	}
-
 	/* *cnt1+=1;*/
 	for(k = lo[2]; k<=hi[2]; k++) {
 	  kloc = k-lo[2];
 	  for(l = lo[3]; l<=hi[3]; l++) {
 	    lloc = l-lo[3];
-	    if (s_ij[jloc][iloc]*s_kl[lloc][kloc] < tol2e) {
+	    if (s_ij->buf[jloc][iloc] * s_kl->buf[lloc][kloc] < tol2e) {
 	      *icut2 = *icut2 + 1;
 	    }
 	    else  {
-	      if(done_get1==0) {
-		int _lo[4] = {lo[3], lo[2], lo[1], lo[0]};
-		int _hi[4] = {hi[3], hi[2], hi[1], hi[0]};
-		int _lo_jl[2] = {lo[3], lo[1]};
-		int _hi_jl[2] = {hi[3], hi[1]};
-		//NGA_Get(g_schwarz, &_lo[2], &_hi[2], s_ij, &ich);
-		//NGA_Get(g_schwarz, &_lo[0], &_hi[0], s_kl, &ich);
-		NGA_Get(g_dens,&_lo[0],&_hi[0], d_kl,&ich);
-		NGA_Get(g_dens,_lo_jl,_hi_jl, d_jl,&ich);
-		done_get1=1;
-	      }
-
 	      Integer _i=i+1, _j=j+1, _k=k+1, _l=l+1;
 	      g_(&gg, &_i, &_j, &_k, &_l);
 	      /* *cnt2 += 1;*/
-	      f_ij[jloc][iloc] = f_ij[jloc][iloc] + gg*d_kl[lloc][kloc];
-	      f_ik[kloc][iloc] = f_ik[kloc][iloc] - 0.5*gg*d_jl[lloc][jloc];
-	      accum=1;
+	      f_ij->buf[jloc][iloc] = f_ij->buf[jloc][iloc] + gg*d_kl->buf[lloc][kloc];
+	      f_ik->buf[kloc][iloc] = f_ik->buf[kloc][iloc] - 0.5*gg*d_jl->buf[lloc][jloc];
 	      *icut3 = *icut3 + 1;
 	    }
 	  }
@@ -487,16 +463,5 @@ static void twoel_task(UniformTaskCollection *utc, void *_bigd, int bigd_len,
       }
     }
   }
-  if(accum)
-  {
-    /*lkji*/
-    int _lo[2] = {lo[1], lo[0]};
-    int _hi[2] = {hi[1], hi[0]};
-    int _lo_ik[2] = {lo[2], lo[0]};
-    int _hi_ik[2] = {hi[2], hi[0]};
-    NGA_Acc(g_fock,_lo,_hi, f_ij,&ich,&one);
-    NGA_Acc(g_fock,_lo_ik,_hi_ik, f_ik,&ich,&one);
-  }
 }
-
 
