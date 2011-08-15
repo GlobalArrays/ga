7eb7171aebe153b0d2a4c0a1dc8ad09826137637
Jeff Daily <jeff.daily@pnnl.gov>
2011-08-15 18:43:10 +0000

    Sort and reformat parmci.h. Generate armci's capi.c from parmci.h.

--- armci/src/common/capi.c
+++ armci/src/common/capi.c
@@ -1,421 +1,451 @@
+
 #if HAVE_CONFIG_H
 #   include "config.h"
 #endif
 
+#include <stdio.h>
 
-#if HAVE_STDIO_H
-#   include <stdio.h>
-#endif
 #include "armci.h"
 #include "parmci.h"
 
-int
-ARMCI_AccV (int op, void *scale, armci_giov_t * darr, int len, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Acc
+#endif
+int ARMCI_Acc(int optype, void *scale, void *src, void *dst, int bytes, int proc)
 {
-  int rval;
-  rval = PARMCI_AccV (op, scale, darr, len, proc);
-  return rval;
+    return PARMCI_Acc(optype, scale, src, dst, bytes, proc);
 }
 
-void
-ARMCI_Barrier ()
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_AccS
+#endif
+int ARMCI_AccS(int optype, void *scale, void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc)
 {
-  PARMCI_Barrier ();
+    return PARMCI_AccS(optype, scale, src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count, stride_levels, proc);
 }
 
-int
-ARMCI_AccS (int optype, void *scale, void *src_ptr, int *src_stride_arr,
-	    void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels,
-	    int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_AccV
+#endif
+int ARMCI_AccV(int op, void *scale, armci_giov_t *darr, int len, int proc)
 {
-  int rval;
-  rval =
-    PARMCI_AccS (optype, scale, src_ptr, src_stride_arr, dst_ptr,
-		 dst_stride_arr, count, stride_levels, proc);
-  return rval;
+    return PARMCI_AccV(op, scale, darr, len, proc);
 }
 
-void
-ARMCI_Finalize ()
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_AllFence
+#endif
+void ARMCI_AllFence()
 {
-  PARMCI_Finalize ();
+    PARMCI_AllFence();
 }
 
-int
-ARMCI_NbPut (void *src, void *dst, int bytes, int proc,
-	     armci_hdl_t * nb_handle)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Barrier
+#endif
+void ARMCI_Barrier()
 {
-  int rval;
-  rval = PARMCI_NbPut (src, dst, bytes, proc, nb_handle);
-  return rval;
+    PARMCI_Barrier();
 }
 
-int
-ARMCI_GetValueInt (void *src, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Create_mutexes
+#endif
+int ARMCI_Create_mutexes(int num)
 {
-  int rval;
-  rval = PARMCI_GetValueInt (src, proc);
-  return rval;
+    return PARMCI_Create_mutexes(num);
 }
 
-int
-ARMCI_Put_flag (void *src, void *dst, int bytes, int *f, int v, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Destroy_mutexes
+#endif
+int ARMCI_Destroy_mutexes()
 {
-  int rval;
-  rval = PARMCI_Put_flag (src, dst, bytes, f, v, proc);
-  return rval;
+    return PARMCI_Destroy_mutexes();
 }
 
-int
-ARMCI_NbGetS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-	      int *dst_stride_arr, int *count, int stride_levels, int proc,
-	      armci_hdl_t * nb_handle)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Fence
+#endif
+void ARMCI_Fence(int proc)
 {
-  int rval;
-  rval =
-    PARMCI_NbGetS (src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count,
-		   stride_levels, proc, nb_handle);
-  return rval;
+    PARMCI_Fence(proc);
 }
 
-void *
-ARMCI_Malloc_local (armci_size_t bytes)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Finalize
+#endif
+void ARMCI_Finalize()
 {
-  void *rval;
-  rval = PARMCI_Malloc_local (bytes);
-  return rval;
+    PARMCI_Finalize();
 }
 
-int
-ARMCI_Free_local (void *ptr)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Free
+#endif
+int ARMCI_Free(void *ptr)
 {
-  int rval;
-  rval = PARMCI_Free_local (ptr);
-  return rval;
+    return PARMCI_Free(ptr);
 }
 
-int
-ARMCI_Get (void *src, void *dst, int bytes, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Free_local
+#endif
+int ARMCI_Free_local(void *ptr)
 {
-  int rval;
-  rval = PARMCI_Get (src, dst, bytes, proc);
-  return rval;
+    return PARMCI_Free_local(ptr);
 }
 
-int
-ARMCI_Put (void *src, void *dst, int bytes, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Get
+#endif
+int ARMCI_Get(void *src, void *dst, int bytes, int proc)
 {
-  int rval;
-  rval = PARMCI_Put (src, dst, bytes, proc);
-  return rval;
+    return PARMCI_Get(src, dst, bytes, proc);
 }
 
-int
-ARMCI_Acc(int optype, void *scale, void *src, void* dst, int bytes, int proc) 
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_GetS
+#endif
+int ARMCI_GetS(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc)
 {
-  int rval;
-  rval = PARMCI_Acc(optype, scale, src, dst, bytes, proc);
-  return rval;
+    return PARMCI_GetS(src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count, stride_levels, proc);
 }
 
-int
-ARMCI_Destroy_mutexes ()
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_GetV
+#endif
+int ARMCI_GetV(armci_giov_t *darr, int len, int proc)
 {
-  int rval;
-  rval = PARMCI_Destroy_mutexes ();
-  return rval;
+    return PARMCI_GetV(darr, len, proc);
 }
 
-int
-ARMCI_GetS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-	    int *dst_stride_arr, int *count, int stride_levels, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_GetValueDouble
+#endif
+double ARMCI_GetValueDouble(void *src, int proc)
 {
-  int rval;
-  rval =
-    PARMCI_GetS (src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count,
-		 stride_levels, proc);
-  return rval;
+    return PARMCI_GetValueDouble(src, proc);
 }
 
-int
-ARMCI_NbAccV (int op, void *scale, armci_giov_t * darr, int len, int proc,
-	      armci_hdl_t * nb_handle)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_GetValueFloat
+#endif
+float ARMCI_GetValueFloat(void *src, int proc)
 {
-  int rval;
-  rval = PARMCI_NbAccV (op, scale, darr, len, proc, nb_handle);
-  return rval;
+    return PARMCI_GetValueFloat(src, proc);
 }
 
-float
-ARMCI_GetValueFloat (void *src, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_GetValueInt
+#endif
+int ARMCI_GetValueInt(void *src, int proc)
 {
-  float rval;
-  rval = PARMCI_GetValueFloat (src, proc);
-  return rval;
+    return PARMCI_GetValueInt(src, proc);
 }
 
-int
-ARMCI_Malloc (void **ptr_arr, armci_size_t bytes)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_GetValueLong
+#endif
+long ARMCI_GetValueLong(void *src, int proc)
 {
-  int rval;
-  rval = PARMCI_Malloc (ptr_arr, bytes);
-  return rval;
+    return PARMCI_GetValueLong(src, proc);
 }
 
-int
-ARMCI_NbAccS (int optype, void *scale, void *src_ptr, int *src_stride_arr,
-	      void *dst_ptr, int *dst_stride_arr, int *count,
-	      int stride_levels, int proc, armci_hdl_t * nb_handle)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Init
+#endif
+int ARMCI_Init()
 {
-  int rval;
-  rval =
-    PARMCI_NbAccS (optype, scale, src_ptr, src_stride_arr, dst_ptr,
-		   dst_stride_arr, count, stride_levels, proc, nb_handle);
-  return rval;
+    return PARMCI_Init();
 }
 
-int
-ARMCI_PutS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-	    int *dst_stride_arr, int *count, int stride_levels, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Init_args
+#endif
+int ARMCI_Init_args(int *argc, char ***argv)
 {
-  int rval;
-  rval =
-    PARMCI_PutS (src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count,
-		 stride_levels, proc);
-  return rval;
+    return PARMCI_Init_args(argc, argv);
 }
 
-void *
-ARMCI_Memat (armci_meminfo_t * meminfo, long offset)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Lock
+#endif
+void ARMCI_Lock(int mutex, int proc)
 {
-  void *rval;
-  rval = PARMCI_Memat (meminfo, offset);
-  return rval;
+    PARMCI_Lock(mutex, proc);
 }
 
-int
-ARMCI_PutV (armci_giov_t * darr, int len, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Malloc
+#endif
+int ARMCI_Malloc(void **ptr_arr, armci_size_t bytes)
 {
-  int rval;
-  rval = PARMCI_PutV (darr, len, proc);
-  return rval;
+    return PARMCI_Malloc(ptr_arr, bytes);
 }
 
-int
-ARMCI_Free (void *ptr)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Malloc_local
+#endif
+void* ARMCI_Malloc_local(armci_size_t bytes)
 {
-  int rval;
-  rval = PARMCI_Free (ptr);
-  return rval;
+    return PARMCI_Malloc_local(bytes);
 }
 
-int
-ARMCI_Init_args (int *argc, char ***argv)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Memat
+#endif
+void* ARMCI_Memat(armci_meminfo_t *meminfo, long offset)
 {
-  int rval;
-  rval = PARMCI_Init_args (argc, argv);
-  return rval;
+    return PARMCI_Memat(meminfo, offset);
 }
 
-int
-ARMCI_PutValueInt (int src, void *dst, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Memget
+#endif
+void ARMCI_Memget(size_t bytes, armci_meminfo_t *meminfo, int memflg)
 {
-  int rval;
-  rval = PARMCI_PutValueInt (src, dst, proc);
-  return rval;
+    PARMCI_Memget(bytes, meminfo, memflg);
 }
 
-void
-ARMCI_Memget (size_t bytes, armci_meminfo_t * meminfo, int memflg)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_NbAccS
+#endif
+int ARMCI_NbAccS(int optype, void *scale, void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc, armci_hdl_t *nb_handle)
 {
-  PARMCI_Memget (bytes, meminfo, memflg);
+    return PARMCI_NbAccS(optype, scale, src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count, stride_levels, proc, nb_handle);
 }
 
-void
-ARMCI_AllFence ()
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_NbAccV
+#endif
+int ARMCI_NbAccV(int op, void *scale, armci_giov_t *darr, int len, int proc, armci_hdl_t *nb_handle)
 {
-  PARMCI_AllFence ();
+    return PARMCI_NbAccV(op, scale, darr, len, proc, nb_handle);
 }
 
-int
-ARMCI_NbPutV (armci_giov_t * darr, int len, int proc, armci_hdl_t * nb_handle)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_NbGet
+#endif
+int ARMCI_NbGet(void *src, void *dst, int bytes, int proc, armci_hdl_t *nb_handle)
 {
-  int rval;
-  rval = PARMCI_NbPutV (darr, len, proc, nb_handle);
-  return rval;
+    return PARMCI_NbGet(src, dst, bytes, proc, nb_handle);
 }
 
-int
-ARMCI_PutValueDouble (double src, void *dst, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_NbGetS
+#endif
+int ARMCI_NbGetS(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc, armci_hdl_t *nb_handle)
 {
-  int rval;
-  rval = PARMCI_PutValueDouble (src, dst, proc);
-  return rval;
+    return PARMCI_NbGetS(src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count, stride_levels, proc, nb_handle);
 }
 
-int
-ARMCI_GetV (armci_giov_t * darr, int len, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_NbGetV
+#endif
+int ARMCI_NbGetV(armci_giov_t *darr, int len, int proc, armci_hdl_t *nb_handle)
 {
-  int rval;
-  rval = PARMCI_GetV (darr, len, proc);
-  return rval;
+    return PARMCI_NbGetV(darr, len, proc, nb_handle);
 }
 
-int
-ARMCI_Test (armci_hdl_t * nb_handle)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_NbPut
+#endif
+int ARMCI_NbPut(void *src, void *dst, int bytes, int proc, armci_hdl_t *nb_handle)
 {
-  int rval;
-  rval = PARMCI_Test (nb_handle);
-  return rval;
+    return PARMCI_NbPut(src, dst, bytes, proc, nb_handle);
 }
 
-void
-ARMCI_Unlock (int mutex, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_NbPutS
+#endif
+int ARMCI_NbPutS(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc, armci_hdl_t *nb_handle)
 {
-  PARMCI_Unlock (mutex, proc);
+    return PARMCI_NbPutS(src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count, stride_levels, proc, nb_handle);
 }
 
-void
-ARMCI_Fence (int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_NbPutV
+#endif
+int ARMCI_NbPutV(armci_giov_t *darr, int len, int proc, armci_hdl_t *nb_handle)
 {
-  PARMCI_Fence (proc);
+    return PARMCI_NbPutV(darr, len, proc, nb_handle);
 }
 
-int
-ARMCI_Create_mutexes (int num)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Put
+#endif
+int ARMCI_Put(void *src, void *dst, int bytes, int proc)
 {
-  int rval;
-  rval = PARMCI_Create_mutexes (num);
-  return rval;
+    return PARMCI_Put(src, dst, bytes, proc);
 }
 
-int
-ARMCI_PutS_flag (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-		 int *dst_stride_arr, int *count, int stride_levels,
-		 int *flag, int val, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_PutS
+#endif
+int ARMCI_PutS(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc)
 {
-  int rval;
-  rval =
-    PARMCI_PutS_flag (src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count,
-		      stride_levels, flag, val, proc);
-  return rval;
+    return PARMCI_PutS(src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count, stride_levels, proc);
 }
 
-int
-ARMCI_WaitProc (int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_PutS_flag
+#endif
+int ARMCI_PutS_flag(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int *flag, int val, int proc)
 {
-  int rval;
-  rval = PARMCI_WaitProc (proc);
-  return rval;
+    return PARMCI_PutS_flag(src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count, stride_levels, flag, val, proc);
 }
 
-void
-ARMCI_Lock (int mutex, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_PutS_flag_dir
+#endif
+int ARMCI_PutS_flag_dir(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int *flag, int val, int proc)
 {
-  PARMCI_Lock (mutex, proc);
+    return PARMCI_PutS_flag_dir(src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count, stride_levels, flag, val, proc);
 }
 
-double
-ARMCI_GetValueDouble (void *src, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_PutV
+#endif
+int ARMCI_PutV(armci_giov_t *darr, int len, int proc)
 {
-  double rval;
-  rval = PARMCI_GetValueDouble (src, proc);
-  return rval;
+    return PARMCI_PutV(darr, len, proc);
 }
 
-int
-ARMCI_NbGetV (armci_giov_t * darr, int len, int proc, armci_hdl_t * nb_handle)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_PutValueDouble
+#endif
+int ARMCI_PutValueDouble(double src, void *dst, int proc)
 {
-  int rval;
-  rval = PARMCI_NbGetV (darr, len, proc, nb_handle);
-  return rval;
+    return PARMCI_PutValueDouble(src, dst, proc);
 }
 
-int
-ARMCI_Rmw (int op, void *ploc, void *prem, int extra, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_PutValueFloat
+#endif
+int ARMCI_PutValueFloat(float src, void *dst, int proc)
 {
-  int rval;
-  rval = PARMCI_Rmw (op, ploc, prem, extra, proc);
-  return rval;
+    return PARMCI_PutValueFloat(src, dst, proc);
 }
 
-int
-ARMCI_Init ()
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_PutValueInt
+#endif
+int ARMCI_PutValueInt(int src, void *dst, int proc)
 {
-  int rval;
-  rval = PARMCI_Init ();
-  return rval;
+    return PARMCI_PutValueInt(src, dst, proc);
 }
 
-int
-ARMCI_WaitAll ()
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_PutValueLong
+#endif
+int ARMCI_PutValueLong(long src, void *dst, int proc)
 {
-  int rval;
-  rval = PARMCI_WaitAll ();
-  return rval;
+    return PARMCI_PutValueLong(src, dst, proc);
 }
 
-int
-ARMCI_NbGet (void *src, void *dst, int bytes, int proc,
-	     armci_hdl_t * nb_handle)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Put_flag
+#endif
+int ARMCI_Put_flag(void *src, void *dst, int bytes, int *f, int v, int proc)
 {
-  int rval;
-  rval = PARMCI_NbGet (src, dst, bytes, proc, nb_handle);
-  return rval;
+    return PARMCI_Put_flag(src, dst, bytes, f, v, proc);
 }
 
-int
-ARMCI_PutValueFloat (float src, void *dst, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Rmw
+#endif
+int ARMCI_Rmw(int op, void *ploc, void *prem, int extra, int proc)
 {
-  int rval;
-  rval = PARMCI_PutValueFloat (src, dst, proc);
-  return rval;
+    return PARMCI_Rmw(op, ploc, prem, extra, proc);
 }
 
-int
-ARMCI_NbPutS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-	      int *dst_stride_arr, int *count, int stride_levels, int proc,
-	      armci_hdl_t * nb_handle)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Test
+#endif
+int ARMCI_Test(armci_hdl_t *nb_handle)
 {
-  int rval;
-  rval =
-    PARMCI_NbPutS (src_ptr, src_stride_arr, dst_ptr, dst_stride_arr, count,
-		   stride_levels, proc, nb_handle);
-  return rval;
+    return PARMCI_Test(nb_handle);
 }
 
-int
-ARMCI_PutS_flag_dir (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-		     int *dst_stride_arr, int *count, int stride_levels,
-		     int *flag, int val, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Unlock
+#endif
+void ARMCI_Unlock(int mutex, int proc)
 {
-  int rval;
-  rval =
-    PARMCI_PutS_flag_dir (src_ptr, src_stride_arr, dst_ptr, dst_stride_arr,
-			  count, stride_levels, flag, val, proc);
-  return rval;
+    PARMCI_Unlock(mutex, proc);
 }
 
-int
-ARMCI_PutValueLong (long src, void *dst, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_Wait
+#endif
+int ARMCI_Wait(armci_hdl_t *nb_handle)
 {
-  int rval;
-  rval = PARMCI_PutValueLong (src, dst, proc);
-  return rval;
+    return PARMCI_Wait(nb_handle);
 }
 
-int
-ARMCI_Wait (armci_hdl_t * nb_handle)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_WaitAll
+#endif
+int ARMCI_WaitAll()
 {
-  int rval;
-  rval = PARMCI_Wait (nb_handle);
-  return rval;
+    return PARMCI_WaitAll();
 }
 
-long
-ARMCI_GetValueLong (void *src, int proc)
+
+#if HAVE_SYS_WEAK_ALIAS_PRAGMA
+#   pragma weak ARMCI_WaitProc
+#endif
+int ARMCI_WaitProc(int proc)
 {
-  long rval;
-  rval = PARMCI_GetValueLong (src, proc);
-  return rval;
+    return PARMCI_WaitProc(proc);
 }
+
--- armci/src/include/parmci.h
+++ armci/src/include/parmci.h
@@ -1,122 +1,56 @@
+#ifndef _PARMCI_H_
+#define _PARMCI_H_
+
 #include "armci.h"
 
-int PARMCI_Acc(int optype, void *scale, void *src, void* dst, int bytes, int proc);
+extern int    PARMCI_Acc(int optype, void *scale, void *src, void* dst, int bytes, int proc);
+extern int    PARMCI_AccS(int optype, void *scale, void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc);
+extern int    PARMCI_AccV(int op, void *scale, armci_giov_t * darr, int len, int proc);
+extern void   PARMCI_AllFence();
+extern void   PARMCI_Barrier();
+extern int    PARMCI_Create_mutexes(int num);
+extern int    PARMCI_Destroy_mutexes();
+extern void   PARMCI_Fence(int proc);
+extern void   PARMCI_Finalize();
+extern int    PARMCI_Free_local(void *ptr);
+extern int    PARMCI_Free(void *ptr);
+extern int    PARMCI_GetS(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc);
+extern double PARMCI_GetValueDouble(void *src, int proc);
+extern float  PARMCI_GetValueFloat(void *src, int proc);
+extern int    PARMCI_GetValueInt(void *src, int proc);
+extern long   PARMCI_GetValueLong(void *src, int proc);
+extern int    PARMCI_GetV(armci_giov_t * darr, int len, int proc);
+extern int    PARMCI_Get(void *src, void *dst, int bytes, int proc);
+extern int    PARMCI_Init();
+extern int    PARMCI_Init_args(int *argc, char ***argv);
+extern void   PARMCI_Lock(int mutex, int proc);
+extern void*  PARMCI_Malloc_local(armci_size_t bytes);
+extern int    PARMCI_Malloc(void **ptr_arr, armci_size_t bytes);
+extern void*  PARMCI_Memat(armci_meminfo_t * meminfo, long offset);
+extern void   PARMCI_Memget(size_t bytes, armci_meminfo_t * meminfo, int memflg);
+extern int    PARMCI_NbAccS(int optype, void *scale, void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc, armci_hdl_t * nb_handle);
+extern int    PARMCI_NbAccV(int op, void *scale, armci_giov_t * darr, int len, int proc, armci_hdl_t * nb_handle);
+extern int    PARMCI_NbGetS(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc, armci_hdl_t * nb_handle);
+extern int    PARMCI_NbGetV(armci_giov_t * darr, int len, int proc, armci_hdl_t * nb_handle);
+extern int    PARMCI_NbGet(void *src, void *dst, int bytes, int proc, armci_hdl_t * nb_handle);
+extern int    PARMCI_NbPutS(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc, armci_hdl_t * nb_handle);
+extern int    PARMCI_NbPutV(armci_giov_t * darr, int len, int proc, armci_hdl_t * nb_handle);
+extern int    PARMCI_NbPut(void *src, void *dst, int bytes, int proc, armci_hdl_t * nb_handle);
+extern int    PARMCI_Put_flag(void *src, void *dst, int bytes, int *f, int v, int proc);
+extern int    PARMCI_PutS_flag_dir(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int *flag, int val, int proc);
+extern int    PARMCI_PutS_flag(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int *flag, int val, int proc);
+extern int    PARMCI_PutS(void *src_ptr, int *src_stride_arr, void *dst_ptr, int *dst_stride_arr, int *count, int stride_levels, int proc);
+extern int    PARMCI_PutValueDouble(double src, void *dst, int proc);
+extern int    PARMCI_PutValueFloat(float src, void *dst, int proc);
+extern int    PARMCI_PutValueInt(int src, void *dst, int proc);
+extern int    PARMCI_PutValueLong(long src, void *dst, int proc);
+extern int    PARMCI_PutV(armci_giov_t * darr, int len, int proc);
+extern int    PARMCI_Put(void *src, void *dst, int bytes, int proc);
+extern int    PARMCI_Rmw(int op, void *ploc, void *prem, int extra, int proc);
+extern int    PARMCI_Test(armci_hdl_t * nb_handle);
+extern void   PARMCI_Unlock(int mutex, int proc);
+extern int    PARMCI_WaitAll();
+extern int    PARMCI_Wait(armci_hdl_t * nb_handle);
+extern int    PARMCI_WaitProc(int proc);
 
-int PARMCI_AccV (int op, void *scale, armci_giov_t * darr, int len, int proc);
-
-void PARMCI_Barrier ();
-
-int PARMCI_AccS (int optype, void *scale, void *src_ptr, int *src_stride_arr,
-		 void *dst_ptr, int *dst_stride_arr, int *count,
-		 int stride_levels, int proc);
-
-void PARMCI_Finalize ();
-
-int PARMCI_NbPut (void *src, void *dst, int bytes, int proc,
-		  armci_hdl_t * nb_handle);
-
-int PARMCI_GetValueInt (void *src, int proc);
-
-int PARMCI_Put_flag (void *src, void *dst, int bytes, int *f, int v,
-		     int proc);
-
-int PARMCI_NbGetS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-		   int *dst_stride_arr, int *count, int stride_levels,
-		   int proc, armci_hdl_t * nb_handle);
-
-void *PARMCI_Malloc_local (armci_size_t bytes);
-
-int PARMCI_Free_local (void *ptr);
-
-int PARMCI_Get (void *src, void *dst, int bytes, int proc);
-
-int PARMCI_Put (void *src, void *dst, int bytes, int proc);
-
-int PARMCI_Destroy_mutexes ();
-
-int PARMCI_GetS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-		 int *dst_stride_arr, int *count, int stride_levels,
-		 int proc);
-
-int PARMCI_NbAccV (int op, void *scale, armci_giov_t * darr, int len,
-		   int proc, armci_hdl_t * nb_handle);
-
-float PARMCI_GetValueFloat (void *src, int proc);
-
-int PARMCI_Malloc (void **ptr_arr, armci_size_t bytes);
-
-int PARMCI_NbAccS (int optype, void *scale, void *src_ptr,
-		   int *src_stride_arr, void *dst_ptr, int *dst_stride_arr,
-		   int *count, int stride_levels, int proc,
-		   armci_hdl_t * nb_handle);
-
-int PARMCI_PutS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-		 int *dst_stride_arr, int *count, int stride_levels,
-		 int proc);
-
-void *PARMCI_Memat (armci_meminfo_t * meminfo, long offset);
-
-int PARMCI_PutV (armci_giov_t * darr, int len, int proc);
-
-int PARMCI_Free (void *ptr);
-
-int PARMCI_Init_args (int *argc, char ***argv);
-
-int PARMCI_PutValueInt (int src, void *dst, int proc);
-
-void PARMCI_Memget (size_t bytes, armci_meminfo_t * meminfo, int memflg);
-
-void PARMCI_AllFence ();
-
-int PARMCI_NbPutV (armci_giov_t * darr, int len, int proc,
-		   armci_hdl_t * nb_handle);
-
-int PARMCI_PutValueDouble (double src, void *dst, int proc);
-
-int PARMCI_GetV (armci_giov_t * darr, int len, int proc);
-
-int PARMCI_Test (armci_hdl_t * nb_handle);
-
-void PARMCI_Unlock (int mutex, int proc);
-
-void PARMCI_Fence (int proc);
-
-int PARMCI_Create_mutexes (int num);
-
-int PARMCI_PutS_flag (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-		      int *dst_stride_arr, int *count, int stride_levels,
-		      int *flag, int val, int proc);
-
-int PARMCI_WaitProc (int proc);
-
-void PARMCI_Lock (int mutex, int proc);
-
-double PARMCI_GetValueDouble (void *src, int proc);
-
-int PARMCI_NbGetV (armci_giov_t * darr, int len, int proc,
-		   armci_hdl_t * nb_handle);
-
-int PARMCI_Rmw (int op, void *ploc, void *prem, int extra, int proc);
-
-int PARMCI_Init ();
-
-int PARMCI_WaitAll ();
-
-int PARMCI_NbGet (void *src, void *dst, int bytes, int proc,
-		  armci_hdl_t * nb_handle);
-
-int PARMCI_PutValueFloat (float src, void *dst, int proc);
-
-int PARMCI_NbPutS (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-		   int *dst_stride_arr, int *count, int stride_levels,
-		   int proc, armci_hdl_t * nb_handle);
-
-int PARMCI_PutS_flag_dir (void *src_ptr, int *src_stride_arr, void *dst_ptr,
-			  int *dst_stride_arr, int *count, int stride_levels,
-			  int *flag, int val, int proc);
-
-int PARMCI_PutValueLong (long src, void *dst, int proc);
-
-int PARMCI_Wait (armci_hdl_t * nb_handle);
-
-long PARMCI_GetValueLong (void *src, int proc);
+#endif /* _PARMCI_H_ */
