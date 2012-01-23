59d58e33400cad6f273336bc52f47783ef3a195b
Jeff Daily <jeff.daily@pnnl.gov>
2012-01-23 23:20:45 +0000

    add strided test to GA perf2.x

--- global/testing/perf2.c
+++ global/testing/perf2.c
@@ -41,19 +41,22 @@
 #define OP_GET 0
 #define OP_PUT 1
 #define OP_ACC 2
+#define ENABLE_CLEANUP 0
 
-int CHECK_RESULT = 0;
-int chunk[CHUNK_NUM] = {
+static int CHECK_RESULT = 0;
+static int chunk[CHUNK_NUM] = {
       1,   3,   4,   6,   9,  12,  16,  20,  24,  30,
      40,  48,  52,  64,  78,  91, 104, 128, 142, 171,
     210, 256, 300, 353, 400, 440, 476, 512 };
-int nproc = -1;
-int me = -1;
-int warn_accuracy = 0;
+static int nproc = -1;
+static int me = -1;
+static int warn_accuracy = 0;
 
-double time_op(int g_a, double *buf_, int chunk, int loop, int proc,
-               int ndim, int op);
-void test_1D();
+static void fill_array(double *arr, int count, int which);
+static double time_op(int g_a, double *buf_, int chunk, int loop, int proc,
+                      int ndim, int op);
+static void test_1D();
+static void test_2D();
 
 
 int main(int argc, char **argv)
@@ -84,13 +87,11 @@ int main(int argc, char **argv)
   }
   test_1D();
 
-#if 0
   /* test 2 dimension array */
   if (!me) {
     printf("\n\t\t\tStrided Data Transfer\n");
   }
   test_2D();
-#endif
 
 #if 0
   if (me == 0) {
@@ -126,11 +127,11 @@ double time_op(int g_a, double *buf_, int chunk, int loop, int proc,
   NGA_Distribution(g_a, proc, lo, hi);
   /* determine how much data to grab based on the chunk and dimensionality */
   if (ndim == 1) {
-    hi[0] = lo[0] + chunk*chunk;
+    hi[0] = lo[0] + chunk*chunk - 1;
   }
   else if (ndim == 2) {
-    hi[0] = lo[0] + chunk;
-    hi[1] = lo[1] + chunk;
+    hi[0] = lo[0] + chunk - 1;
+    hi[1] = lo[1] + chunk - 1;
     ld = chunk;
   }
   else {
@@ -232,10 +233,13 @@ void test_1D()
 
       for (dst=1; dst<nproc; ++dst) {
         /* contiguous get */
+        fill_array(buf, SIZE * SIZE, me * 10);
         t_get += time_op(g_a, buf, chunk[i], loop, dst, 1, OP_GET);
         /* contiguous put */
+        fill_array(buf, SIZE * SIZE, me * 10);
         t_put += time_op(g_a, buf, chunk[i], loop, dst, 1, OP_PUT);
         /* contiguous acc */
+        fill_array(buf, SIZE * SIZE, me * 10);
         t_acc += time_op(g_a, buf, chunk[i], loop, dst, 1, OP_ACC);
       }
 
@@ -263,8 +267,10 @@ void test_1D()
 
   GA_Sync();
 
+#if ENABLE_CLEANUP
   /* cleanup */
   GA_Destroy(g_a);
+#endif
 
   if (me == 0) {
 #if MALLOC_LOC
@@ -272,5 +278,116 @@ void test_1D()
 #else
     free(buf);
 #endif
+  }
+}
+
+
+void test_2D()
+{
+  int i = 0;
+  int dst = 0;
+  int g_a = 0;
+  int shape[2] = {SIZE*nproc, SIZE};
+  int dist[2] = {SIZE, SIZE};
+  int lo[2] = {0,0};
+  int hi[2] = {0,0};
+  double *buf = NULL;
+
+  /* allocate the GA */
+  g_a = NGA_Create(C_DBL, 2, shape, "2d", dist);
+  NGA_Distribution(g_a, me, lo, hi);
+  assert(hi[0]-lo[0]+1 == SIZE);
+  assert(hi[1]-lo[1]+1 == SIZE);
+
+  /* memory allocation */
+  if (me == 0) {
+#if MALLOC_LOC
+    buf = (double *)ARMCI_Malloc_local(SIZE * SIZE * sizeof(double));
+    assert(buf != NULL);
+#else
+    buf = (double *)malloc(SIZE * SIZE * sizeof(double));
+    assert(buf != NULL);
+#endif
+  }
+
+  /* only the proc 0 does the work */
+  if (me == 0) {
+    if (!CHECK_RESULT) {
+      printf("  section               get                 put                 acc\n");
+      printf("bytes   loop      usec      MB/s      usec      MB/s      usec      MB/s\n");
+      printf("------- ------  --------  --------  --------  --------  --------  --------\n");
+      fflush(stdout);
+    }
+
+    for (i=0; i<CHUNK_NUM; ++i) {
+      int loop;
+      int bytes = chunk[i] * chunk[i] * sizeof(double);
+      double t_get = 0, t_put = 0, t_acc = 0;
+      double latency_get, latency_put, latency_acc;
+      double bandwidth_get, bandwidth_put, bandwidth_acc;
+
+      loop = (SIZE * SIZE) / (chunk[i] * chunk[i]);
+      loop = (int)sqrt((double)loop);
+      if (loop < 2) {
+        loop = 2;
+      }
+
+      for (dst=1; dst<nproc; ++dst) {
+        /* contiguous get */
+        fill_array(buf, SIZE * SIZE, me * 10);
+        t_get += time_op(g_a, buf, chunk[i], loop, dst, 2, OP_GET);
+        /* contiguous put */
+        fill_array(buf, SIZE * SIZE, me * 10);
+        t_put += time_op(g_a, buf, chunk[i], loop, dst, 2, OP_PUT);
+        /* contiguous acc */
+        fill_array(buf, SIZE * SIZE, me * 10);
+        t_acc += time_op(g_a, buf, chunk[i], loop, dst, 2, OP_ACC);
+      }
+
+      latency_get = t_get / (nproc - 1);
+      latency_put = t_put / (nproc - 1);
+      latency_acc = t_acc / (nproc - 1);
+
+      bandwidth_get = (bytes * (nproc - 1) * 1e-6) / t_get;
+      bandwidth_put = (bytes * (nproc - 1) * 1e-6) / t_put;
+      bandwidth_acc = (bytes * (nproc - 1) * 1e-6) / t_acc;
+
+      /* print */
+      if (!CHECK_RESULT) {
+        printf("%d\t%d\t%.2e  %.2e  %.2e  %.2e  %.2e  %.2e\n",
+               bytes, loop,
+               latency_get / 1e-6, bandwidth_get,
+               latency_put / 1e-6, bandwidth_put,
+               latency_acc / 1e-6, bandwidth_acc);
+      }
+    }
+  }
+  else {
+    sleep(3);
+  }
+
+  GA_Sync();
+
+#if ENABLE_CLEANUP
+  /* cleanup */
+  GA_Destroy(g_a);
+#endif
+
+  if (me == 0) {
+#if MALLOC_LOC
+    ARMCI_Free_local(buf);
+#else
+    free(buf);
+#endif
+  }
+}
+
+
+void fill_array(double *arr, int count, int which)
+{
+  int i;
+
+  for (i = 0; i < count; i++) {
+    arr[i] = i * 8.23 + which * 2.89;
   }
 }
