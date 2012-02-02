1fff8efd56ce070e81538f828ec611bcd4d7ea19
GA Developer <hpctools@googlegroups.com>
2012-02-02 18:33:40 +0000

    RB: Initial test commit of a GA unit test

---
+++ global/testing/unit-tests/ga_unit.h
@@ -0,0 +1,20 @@
+
+#ifndef GA_UNIT_H
+#define GA_UNIT_H
+
+#define GA_PRINT_MSG() printf("Test Completed\n")
+
+#define GA_COMPLETE_MSG() printf("Test Completed\n")
+
+#define GA_ERROR_MSG() printf("GA ERROR\n")
+
+#define GA_ERROR_MSG2() printf("GA ERROR\n")
+
+#define NUM_TYPES 7
+int datatypes[NUM_TYPES] = {C_INT, C_FLOAT, C_LONG,  C_DBL, C_LONGLONG, C_DCPL, C_SCPL};
+
+//#define OP_TYPES 6
+//char operators[OP_TYPES] = {'+', '*', 'max', 'min', 'absmax', 'absmin'};
+
+
+#endif
