56201995c746a7c20f0544def85b93f99a021c7d
Jeff Daily <jeff.daily@pnnl.gov>
2013-07-31 22:58:19 +0000

    patch from Andy May to fix shmlimit.c

--- armci/src/common/signaltrap.c
+++ armci/src/common/signaltrap.c
@@ -35,7 +35,7 @@
 #include "armci.h"
 #include "armcip.h"
 
-#define PAUSE_ON_ERROR__
+#define PAUSE_ON_ERROR
 
 #define  Error armci_die 
 #if !defined(armci_die)
--- armci/src/memory/shmlimit.c
+++ armci/src/memory/shmlimit.c
@@ -94,6 +94,7 @@ int armci_child_shmem_init()
 
     int y;
     int fd[2];
+    int val;
 
     if(pipe(fd)==-1) armci_die("armci shmem_test pipe failed",0);
 
@@ -111,20 +112,21 @@ int armci_child_shmem_init()
        /* due to a bug in OSF1 V4.0/1229/alpha first item written gets hosed*/
        for(i=0;i<2;i++)
 #endif
-           if(write(fd[1],&x,sizeof(int)) < ((ssize_t)sizeof(int)))
+           val=write(fd[1],&x,sizeof(int));
+           if(val < 0 || (size_t)val < sizeof(int))
                          armci_die("armci shmem_test: write failed",0);
        _exit(0);
 
     }else{
 
        pid_t rc;
-       int val;
 
 #ifdef PIPE_AFTER_FORK_BUG
        /* due to a bug in OSF1 V4.0/1229/alpha first item read is garbage */
        for(i=0;i<2;i++)
 #endif
-          if((val=read(fd[0],&y,sizeof(int))) < ((ssize_t)sizeof(int)))
+          val=read(fd[0],&y,sizeof(int));
+          if(val < 0 || (size_t)val < sizeof(int))
                          armci_die("armci shmem_test: read failed",val);
 
 #ifdef SOLARIS
@@ -147,3 +149,4 @@ again:   rc = wait (&status);
        printf("%d:in parent: x=%d y=%d\n",armci_me,x,y);fflush(stdout);sleep(1);
     return y;
 }
+
