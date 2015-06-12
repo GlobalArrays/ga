cad274e09bcc946c0e6d483d01a29c1985adf539
Jeff Daily <jeff.daily@pnnl.gov>
2015-06-12 23:04:38 +0000

    guard C++ mangling of ga-mpi.h

--- global/src/ga-mpi.h
+++ global/src/ga-mpi.h
@@ -3,8 +3,16 @@
 
 #include <mpi.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern MPI_Comm GA_MPI_Comm();
 extern MPI_Comm GA_MPI_Comm_pgroup(int pgroup);
 extern MPI_Comm GA_MPI_Comm_pgroup_default();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* GA_MPI_H_ */
