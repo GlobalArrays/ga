e554547931e25b950a2cd7a420a7f8f2761cc50b
Bruce Palmer <Bruce.Palmer@pnnl.gov>
2016-06-29 18:15:01 +0000

    Got rid of unused preprocessor symbols

--- pario/dra/rate.c
+++ pario/dra/rate.c
@@ -73,13 +73,7 @@
 
 #define MULTFILES 0
 
-#ifdef SOLARIS
-#   if MULTFILES
-#       define USEMULTFILES 1
-#   endif
-#else
 #   define USEMULTFILES 0
-#endif
 
 #define IA 16807
 #define IM 2147483647
