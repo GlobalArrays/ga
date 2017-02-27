231bab4a830ff82a2f84830c74f487d5fca9c137
Jeff Daily <jeff.daily@pnnl.gov>
2017-02-27 12:55:17 -0800

    add travis integration from https://github.com/jeffhammond/armci-mpi

---
+++ travis/build-run.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+# Exit on error
+set -ev
+
+os=`uname`
+TRAVIS_ROOT="$1"
+MPI_IMPL="$2"
+
+# Environment variables
+export CFLAGS="-std=c99"
+#export MPICH_CC=$CC
+export MPICC=mpicc
+
+case "$os" in
+    Darwin)
+        ;;
+    Linux)
+       export PATH=$TRAVIS_ROOT/mpich/bin:$PATH
+       export PATH=$TRAVIS_ROOT/open-mpi/bin:$PATH
+       ;;
+esac
+
+# Capture details of build
+case "$MPI_IMPL" in
+    mpich)
+        mpichversion
+        mpicc -show
+        ;;
+    openmpi)
+        # this is missing with Mac build it seems
+        #ompi_info --arch --config
+        mpicc --showme:command
+        ;;
+esac
+
+# Configure and build
+./autogen.sh
+./configure --disable-static
+
+# Run unit tests
+make check
