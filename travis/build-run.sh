43f9f058b3c8b9ba941d6388140a9bcc3055854e
Jeff Daily <jeff.daily@pnnl.gov>
2017-03-17 08:45:05 -0700

    openmpi on osx needs a different TMPDIR

--- travis/build-run.sh
+++ travis/build-run.sh
@@ -35,6 +35,9 @@ case "$MPI_IMPL" in
         case "$os" in
             Darwin)
                 echo "Mac"
+                # Open MPI 2.0.x / v2.1.x won't startup otherwise
+                # https://www.open-mpi.org/faq/?category=osx
+                export TMPDIR=/tmp
             ;;
             Linux)
                 echo "Linux"
