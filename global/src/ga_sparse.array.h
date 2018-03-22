b6a48e5e797f083cae1b2ceeb8d71bbd72047303
bjpalmer <d3g293@prusik.pnl.gov>
2018-03-22 15:44:29 -0700

    Adding more functionality to sparse arrays. The array class builds but has not been tested.

--- global/src/ga_sparse.array.h
+++ global/src/ga_sparse.array.h
@@ -11,7 +11,7 @@ typedef struct {
   Integer g_j;      /* global array containing j indices of sparse matrix */
   Integer g_i;      /* global array containing first j index for row i */
   Integer grp;      /* handle for process group on which array is defined */
-  Integer ilo, ihi; /* minimum and maximum rows indices contained on this process */
+  Integer ilo, ihi; /* minimum and maximum row indices contained on this process */
   Integer nprocs;   /* number of processors containing this array */
   Integer nblocks;  /* number of non-zero sparse blocks contained on this process */
   Integer *blkidx;  /* array containing indices of non-zero blocks */
@@ -21,7 +21,7 @@ typedef struct {
   Integer *jdx;     /* local buffer containing j indices */
   void    *val;     /* local buffer containing values */
   Integer nval;     /* number of values currently stored in local buffers */
-  Ingeger maxval;   /* maximum number of values that can currently */
+  Integer maxval;   /* maximum number of values that can currently */
                     /* be stored in local buffers */
   Integer type;     /* type of data stored in array */
   Integer size;     /* size of data element */
@@ -32,7 +32,9 @@ typedef struct {
 extern _sparse_array *SPA;
 
 extern void sai_init_sparse_arrays();
+/*
 extern void pnga_sprs_array_add_element(Integer s_a, Integer i, Integer j, void *val);
 extern Integer pnga_sprs_array_create(Integer idim, Integer jdim, Integer type);
 extern Integer pnga_sprs_array_assemble(Integer s_a);
+*/
 
