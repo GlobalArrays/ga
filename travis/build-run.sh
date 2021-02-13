369db0a1db71d43346f4a83f444941736df5d429
edoapra <edoardo.apra@gmail.com>
2021-02-13 00:13:19 -0800

    always use -fPIC on Linux

--- travis/build-run.sh
+++ travis/build-run.sh
@@ -58,9 +58,7 @@ case "$os" in
         echo "Mac CFLAGS" $CFLAGS
         ;;
     Linux)
-	if [ $(${CC} -dM -E - </dev/null 2> /dev/null |grep __clang__|head -1|cut -c19) ] ; then
-	    export CFLAGS="${CFLAGS} -fPIC "
-	fi
+	export CFLAGS="${CFLAGS} -fPIC "
         echo "Linux CFLAGS" $CFLAGS
         ;;
 esac
