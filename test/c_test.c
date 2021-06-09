aa9f5acd4b689d61bcaece2f861913a83c4d660c
Bruce J Palmer <d3g293@newell01.pnl.gov>
2021-06-09 10:09:13 -0700

    Updated c_test.c to test double precision arrays.

--- test/c_test.c
+++ test/c_test.c
@@ -6,7 +6,7 @@
 #include "ga.h"
 #include "macdecls.h"
 
-#define DIM 2
+#define DIM 1024
 #define DIMSIZE 4
 #define MAX_FACTOR 256
 
@@ -83,11 +83,13 @@ int main(int argc, char **argv) {
   int dims[2], lo[2], hi[2];
   int tld, tlo[2], thi[2];
   int *buf;
+  double *rbuf;
   int ld;
   int ok;
   int *ptr;
   int nelem;
   int one;
+  double one_r;
   
 
   MPI_Init(&argc, &argv);
@@ -100,6 +102,10 @@ int main(int argc, char **argv) {
 
   /* Divide matrix up into pieces that are owned by each processor */
   factor(nprocs, &pdx, &pdy);
+  if (rank == 0) {
+    printf("  Test run on %d procs configured on %d X %d grid\n",nprocs,pdx,pdy);
+  }
+  if (rank == 0) printf("  Testing integer arrays\n");
 
   ndim = 2;
   dims[0] = DIMSIZE;
@@ -247,6 +253,105 @@ int main(int argc, char **argv) {
     if (rank == 0) printf("Acc is okay\n");
   }
 
+  free(buf);
+  GA_Destroy(g_a);
+  if (rank == 0) printf("Completed GA destroy\n");
+
+  if (rank == 0) printf("  Testing double precision arrays\n");
+  /* create a global array and initialize it to zero */
+  g_a = NGA_Create_handle();
+  if (rank == 0) printf("Created GA handle\n");
+  NGA_Set_data(g_a, ndim, dims, C_DBL);
+  if (rank == 0) printf("Set GA data\n");
+  NGA_Set_device(g_a, 1);
+  if (rank == 0) printf("Set GA device\n");
+  NGA_Allocate(g_a);
+  if (rank == 0) printf("Completed GA allocate\n");
+  GA_Zero(g_a);
+
+  /* allocate a local buffer and initialize it with values*/
+  nsize = (hi[0]-lo[0]+1)*(hi[1]-lo[1]+1);
+  rbuf = (double*)malloc(nsize*sizeof(double));
+  ld = (hi[1]-lo[1]+1);
+  for (ii = lo[0]; ii<=hi[0]; ii++) {
+    i = ii-lo[0];
+    for (jj = lo[1]; jj<=hi[1]; jj++) {
+      j = jj-lo[1];
+      idx = i*ld+j;
+      rbuf[idx] = (double)(ii*DIMSIZE+jj);
+    }
+  }
+
+  /* copy data to global array */
+  NGA_Put(g_a, lo, hi, rbuf, &ld);
+  GA_Sync();
+  if (rank == 0) printf("Completed GA put\n");
+  NGA_Distribution(g_a,rank,tlo,thi);
+
+  /* zero out local buffer */
+  for (i=0; i<nsize; i++) rbuf[i] = 0.0;
+
+  /* copy data from global array to local buffer */
+  if (rank == 0) printf("Calling GA get\n",rank);
+  NGA_Get(g_a, lo, hi, rbuf, &ld);
+  GA_Sync();
+
+  ok = 1;
+  for (ii = lo[0]; ii<=hi[0]; ii++) {
+    i = ii-lo[0];
+    for (jj = lo[1]; jj<=hi[1]; jj++) {
+      j = jj-lo[1];
+      idx = i*ld+j;
+      if (rbuf[idx] != (double)(ii*DIMSIZE+jj)) {
+        if (ok) printf("p[%d] (%d,%d) expected: %d actual[%d]: %f\n",rank,ii,jj,ii*DIMSIZE+jj,
+            idx,rbuf[idx]);
+        ok = 0;
+      }
+    }
+  }
+  if (!ok) {
+    printf("Mismatch found on process %d after Get\n",rank);
+  } else {
+    if (rank == 0) printf("Get is okay\n");
+  }
+
+  /* reset values in buf */
+  for (ii = lo[0]; ii<=hi[0]; ii++) {
+    i = ii-lo[0];
+    for (jj = lo[1]; jj<=hi[1]; jj++) {
+      j = jj-lo[1];
+      idx = i*ld+j;
+      rbuf[idx] = (double)(ii*DIMSIZE+jj);
+    }
+  }
+
+  /* accumulate data to global array */
+  if (rank == 0) printf("Calling GA acc\n",rank);
+  one_r = 1.0;
+  NGA_Acc(g_a, lo, hi, rbuf, &ld, &one_r);
+  GA_Sync();
+  if (rank == 0) printf("Calling GA get\n",rank);
+  NGA_Get(g_a, lo, hi, rbuf, &ld);
+  ok = 1;
+  for (ii = lo[0]; ii<=hi[0]; ii++) {
+    i = ii-lo[0];
+    for (jj = lo[1]; jj<=hi[1]; jj++) {
+      j = jj-lo[1];
+      idx = i*ld+j;
+      if (rbuf[idx] != (double)(2*(ii*DIMSIZE+jj))) {
+        if (ok) printf("p[%d] (%d,%d) expected: %d actual[%d]: %f\n",rank,ii,jj,
+            2*(ii*DIMSIZE+jj),idx,rbuf[idx]);
+        ok = 0;
+      }
+    }
+  }
+  if (!ok) {
+    printf("Mismatch found on process %d after Acc\n",rank);
+  } else {
+    if (rank == 0) printf("Acc is okay\n");
+  }
+
+  free(rbuf);
   GA_Destroy(g_a);
   if (rank == 0) printf("Completed GA destroy\n");
   GA_Terminate();
