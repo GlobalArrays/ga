4f0dda21f340258e3c98c02159c5147875cf85b6
edoapra <edoardo.apra@gmail.com>
2022-02-08 16:03:59 -0800

    fix memory leak

--- comex/src-armci/armci.c
+++ comex/src-armci/armci.c
@@ -91,6 +91,7 @@ void armci_init_domains(MPI_Comm comm)
       nodelist[i] = _my_node_id*_number_of_procs_per_node+i;
     comex_group_create(_number_of_procs_per_node, nodelist,
         COMEX_GROUP_WORLD, &ARMCI_Node_group);
+    free(nodelist);
   }
 }
 
