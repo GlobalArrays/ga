226b129bd7b63f53a8dff583fa33b43d1ba57c49
edoapra <edoardo.apra@gmail.com>
2022-02-08 17:56:27 -0800

    fix for memory leak

--- comex/src-mpi-pt/comex.c
+++ comex/src-mpi-pt/comex.c
@@ -2432,7 +2432,7 @@ STATIC void _put_packed_handler(header_t *header, int proc)
 
     unpack(packed_buffer, mapped_offset,
             stride->stride, stride->count, stride->stride_levels);
-
+    free(stride);
     if ((unsigned)header->length > COMEX_STATIC_BUFFER_SIZE) {
         free(packed_buffer);
     }
