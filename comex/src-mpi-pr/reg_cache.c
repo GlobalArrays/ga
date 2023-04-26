29d2bb48da48d5d1f1b90c1b02680e4e5c776879
Nitin Gawande <nitin.gawande@intel.com>
2023-04-26 11:40:12 -0700

    print statements in reg_cache dot c

--- comex/src-mpi-pr/reg_cache.c
+++ comex/src-mpi-pr/reg_cache.c
@@ -387,6 +387,12 @@ reg_cache_find(int rank, void *buf, size_t len)
             g_state.rank, rank, buf, len);
 #endif
 
+    printf("g_state.rank[%d] reg_nprocs[%d] reg_cache_find(rank=%d, buf=%p, len=%d)\n",
+            g_state.rank, reg_nprocs, rank, buf, len);
+
+    printf("g_state.rank[%d] reg_cache_find(rank=%d, runner=%p)\n",
+            g_state.rank, rank, reg_cache[rank]);
+
     /* preconditions */
     COMEX_ASSERT(NULL != reg_cache);
     COMEX_ASSERT(0 <= rank && rank < reg_nprocs);
