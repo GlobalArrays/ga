b63830c7618eeceb40c1a05a72bb6556cabae705
edoapra <edoardo.apra@gmail.com>
2024-04-29 17:10:31 -0700

    revert f8ec0ae41c876dedb8ea21ca8cd39bf6f67eeab6

--- travis/install-libfabric.sh
+++ travis/install-libfabric.sh
@@ -5,14 +5,10 @@ set -x
 
 TRAVIS_ROOT="$1"
 os=`uname`
-if [ "x$os" = "xDarwin" ] ; then
-    brew install libtool
-    which libtool
-fi
 if [ ! -d "$TRAVIS_ROOT/libfabric" ]; then
 	cd $TRAVIS_ROOT
 	#git clone --depth 10 https://github.com/ofiwg/libfabric.git libfabric-source
-	git clone -b 'v1.20.2' --depth 10 https://github.com/ofiwg/libfabric.git libfabric-source
+	git clone -b 'v1.5.1' --depth 10 https://github.com/ofiwg/libfabric.git libfabric-source
 	cd libfabric-source
 	./autogen.sh
 	./configure --prefix=$TRAVIS_ROOT/libfabric
