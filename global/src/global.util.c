7285f5984aa0de02802275d3bf1f961897604457
Ajay Panyala <ajay.panyala@gmail.com>
2024-07-27 10:41:00 -0700

    remove Cray T3D

--- global/src/global.util.c
+++ global/src/global.util.c
@@ -335,11 +335,7 @@ void pnga_error(char *string, Integer icode)
 extern void Error();
 #endif
 
-#ifdef CRAY_T3D 
-#  define FOUT stdout
-#else
-#  define FOUT stderr
-#endif
+#define FOUT stderr
 #define ERR_LEN 400
     int level;
     char error_buffer[ERR_LEN];
